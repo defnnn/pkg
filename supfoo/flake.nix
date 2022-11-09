cb0ae5d0d726aaff22c43fdaa1d08cdcd682ae59
Amanibhavam <iam@defn.sh>
2022-11-09 04:45:18 +0000

    trying to build this with deps

--- supfoo/flake.nix
+++ supfoo/flake.nix
@@ -40,24 +40,20 @@
         stdenv.mkDerivation rec {
           name = "supfoo-${version}";
 
-          version = "9.0.1";
+          version = "9.0.2";
 
-          src = pkgs.fetchurl {
-            url = "https://github.com/tilt-dev/tilt/releases/download/v0.30.10/tilt.0.30.10.linux.x86_64.tar.gz";
-            sha256 = "67133d806f900eef0a36665b39b8c9ef7d70eacb0f4876ede3ce627049aaa6cf";
-          };
+          dontUnpack = true;
 
-          sourceRoot = ".";
+          installPhase = "mkdir -p $out";
 
-          installPhase = ''
-            install -m 0755 -D tilt $out/bin/supfoo
-          '';
+          propagateBuildInputs = [ foo sup home.defaultPackage.${system} ];
 
-          meta = with lib; {
-            homepage = "https://defn.sh/supfoo";
-            description = "packaging binaries with flake";
-            platforms = platforms.linux;
-          };
+          meta = with lib;
+            {
+              homepage = "https://defn.sh/supfoo";
+              description = "packaging binaries with flake";
+              platforms = platforms.linux;
+            };
         };
     }
     );
