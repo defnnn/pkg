4861eed7ea5bc32468881c34b8cb5c8388d2701d
Amanibhavam <iam@defn.sh>
2022-11-09 04:59:13 +0000

    fix a bug

--- supfoo/flake.nix
+++ supfoo/flake.nix
@@ -1,30 +1,53 @@
 {
-  description = "using sup, foo";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     home.url = "path:/home/ubuntu/dev";
-    fooPkg.url = "github:defn/pkg?dir=foo&ref=v0.0.2";
-    supPkg.url = "github:defn/pkg?dir=sup&ref=v0.0.2";
+    foo-pkg.url = "github:defn/pkg?dir=foo&ref=v0.0.2";
+    sup-pkg.url = "github:defn/pkg?dir=sup&ref=v0.0.2";
   };
 
   outputs =
     { self
     , nixpkgs
-    , fooPkg
-    , supPkg
     , flake-utils
     , home
+    , foo-pkg
+    , sup-pkg
     }:
     flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = nixpkgs.legacyPackages.${system};
-      foo = fooPkg.defaultPackage.${system};
-      sup = supPkg.defaultPackage.${system};
+      pkgs = import nixpkgs { inherit system; };
       lib = nixpkgs.lib;
+      foo = foo-pkg.defaultPackage.${system};
+      sup = sup-pkg.defaultPackage.${system};
     in
     {
+      defaultPackage =
+        with import nixpkgs { inherit system; };
+        stdenv.mkDerivation rec {
+          name = "${slug}-${version}";
+
+          slug = "supfoo";
+          version = "9.0.2";
+
+          dontUnpack = true;
+
+          installPhase = "mkdir -p $out";
+
+          propagatedBuildInputs = [
+            pkgs.vim
+            home.defaultPackage.${system}
+          ];
+
+          meta = with lib;
+            {
+              homepage = "https://defn.sh/${slug}";
+              description = "packaging binaries with flake";
+              platforms = platforms.linux;
+            };
+        };
+
       devShell =
         pkgs.mkShell rec {
           buildInputs = [
@@ -35,26 +58,5 @@
           ];
         };
 
-      defaultPackage =
-        with import nixpkgs { inherit system; };
-        stdenv.mkDerivation rec {
-          name = "supfoo-${version}";
-
-          version = "9.0.2";
-
-          dontUnpack = true;
-
-          installPhase = "mkdir -p $out";
-
-          propagateBuildInputs = [ foo sup home.defaultPackage.${system} ];
-
-          meta = with lib;
-            {
-              homepage = "https://defn.sh/supfoo";
-              description = "packaging binaries with flake";
-              platforms = platforms.linux;
-            };
-        };
-    }
-    );
+    });
 }
