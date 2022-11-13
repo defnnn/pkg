8ce712b8c0b0aa1abc9d9a84206790a90f6a1028
Amanibhavam <iam@defn.sh>
2022-11-13 00:36:08 +0000

    builders take args

--- wrapper/flake.nix
+++ wrapper/flake.nix
@@ -27,24 +27,47 @@
               [ other.self.defaultPackage.${system} ]
               ++ pkgs.lib.lists.foldr hasDefaultPackage [ ] inputsList;
           };
-        downloadBuilder = pkgs.stdenv.mkDerivation
-          rec {
-            name = "${site.slug}-${site.version}";
 
-            src = with site.downloads.${system}; pkgs.fetchurl {
-              url = site.url_template site.downloads.${system};
-              inherit sha256;
+        downloadBuilder = { propagatedBuildInputs ? [ ], buildInputs ? [ ] }:
+          pkgs.stdenv.mkDerivation
+            rec {
+              name = "${site.slug}-${site.version}";
+
+              src = with site.downloads.${system}; pkgs.fetchurl {
+                url = site.url_template site.downloads.${system};
+                inherit sha256;
+              };
+
+              sourceRoot = ".";
+
+              buildInputs = nuildInputs;
+              propagatedBuildInputs = propagatedBuildInputs;
+
+              installPhase = site.installPhase { inherit src; };
+
+              meta = with pkgs.lib; with site; {
+                inherit homepage;
+                inherit description;
+              };
             };
 
-            sourceRoot = ".";
+        nullBuilder = { propagatedBuildInputs ? [ ], buildInputs ? [ ] }:
+          pkgs.stdenv.mkDerivation
+            rec {
+              name = "${slug}-${version}";
 
-            installPhase = site.installPhase { inherit src; };
+              dontUnpack = true;
 
-            meta = with pkgs.lib; with site; {
-              inherit homepage;
-              inherit description;
+              propagatedBuildInputs = propagatedBuildInputs;
+              buildInputs = nuildInputs;
+
+              installPhase = "mkdir -p $out";
+
+              meta = with pkgs.lib; with site; {
+                inherit homepage;
+                inherit description;
+              };
             };
-          };
       };
   };
 }
