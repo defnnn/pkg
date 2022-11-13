4ee7e9dcfc9355d04a721a036bb68028e60a7a1b
Amanibhavam <iam@defn.sh>
2022-11-13 00:11:59 +0000

    working

--- wrapper/flake.nix
+++ wrapper/flake.nix
@@ -38,7 +38,7 @@
 
             sourceRoot = ".";
 
-            installPhase = site.installPhase { inherit src; inherit out; };
+            installPhase = site.installPhase { inherit src; };
 
             meta = with pkgs.lib; with site; {
               inherit homepage;
