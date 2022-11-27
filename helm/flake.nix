624792b99ccf09970864cdf9a87758a11e176a45
Amanibhavam <iam@defn.sh>
2022-11-26 17:21:56 -0800

    helm: support default systems

--- helm/flake.nix
+++ helm/flake.nix
@@ -1,39 +1,55 @@
 {
-  description = "helm";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/22.05";
-    flake-utils.url = "github:numtide/flake-utils";
+    dev.url = github:defn/pkg?dir=dev&ref=dev-0.0.1;
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      defaultPackage =
-        with import nixpkgs { inherit system; };
-        stdenv.mkDerivation rec {
-          name = "${slug}-${version}";
+  outputs = inputs: inputs.dev.main {
+    inherit inputs;
 
-          slug = "helm";
-          version = "3.10.1";
-          arch = "amd64"; # arm64
+    config = rec {
+      slug = "helm";
+      version = "3.10.2";
+      homepage = "https://github.com/defn/pkg/tree/master/${slug}";
+      description = "${slug}";
 
-          src = pkgs.fetchurl {
-            url = "https://get.helm.sh/helm-v${version}-linux-${arch}.tar.gz";
-            sha256 = "sha256-wS0s1jjy0Gb+wSPQvX8BDzLGQ6/fKI05pGELH5yzKvM=";
-          };
+      url_template = input: "https://get.helm.sh/helm-v${input.version}-${input.os}-${input.arch}.tar.gz";
 
-          sourceRoot = ".";
+      installPhase = { src }: ''
+        install -m 0755 -d $out $out/bin
+        install -m 0755 */helm $out/bin/helm
+      '';
 
-          installPhase = ''
-            mkdir -p $out/bin
-            install -m 0755 */helm $out/bin/helm
-          '';
-
-          meta = with lib; {
-            homepage = "https://defn.sh/${slug}";
-            description = "${slug}";
-            platforms = platforms.linux;
-          };
+      downloads = {
+        "x86_64-linux" = rec {
+          inherit version;
+          os = "linux";
+          arch = "amd64";
+          sha256 = "sha256-IxWUGhMpHCd9rJ9l516tVjhkQNOQfgVAvxV65w8Yg0c";
         };
-    });
+        "aarch64-linux" = rec {
+          inherit version;
+          os = "linux";
+          arch = "arm64";
+          sha256 = "sha256-V/oXtrsECjeIEWVXpyV58hgOqWILTuiptyROWQHfAuQ";
+        };
+        "x86_64-darwin" = rec {
+          inherit version;
+          os = "darwin";
+          arch = "amd64";
+          sha256 = "sha256-6ImWDkwdfi39uRsQK+z68icAy4bcPjVT2b69e6taOAM";
+        };
+        "aarch64-darwin" = rec {
+          inherit version;
+          os = "darwin";
+          arch = "arm64";
+          sha256 = "sha256-RgRB7qF2TKQ44p+g44qg0mB0AvdTy2VqSrDakiPtpJQ";
+        };
+      };
+    };
+
+    handler = { pkgs, wrap, system }: rec {
+      devShell = wrap.devShell;
+      defaultPackage = wrap.downloadBuilder { };
+    };
+  };
 }
