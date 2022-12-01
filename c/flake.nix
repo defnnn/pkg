b00a022a5a58db3e4b6c1c3e69f138440256d614
Amanibhavam <iam@defn.sh>
2022-12-01 02:53:28 +0000

    c 0.0.1

--- c/flake.nix
+++ c/flake.nix
@@ -1,63 +1,31 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/22.11";
-    flake-utils.url = "github:numtide/flake-utils";
-    cue-pkg.url = "github:defn/pkg/v0.0.5?dir=cue";
-    hof-pkg.url = "github:defn/pkg/v0.0.5?dir=hof";
+    dev.url = github:defn/pkg/dev-0.0.4?dir=dev;
+    cue.url = "github:defn/pkg/cue-0.4.3?dir=cue";
+    hof.url = "github:defn/pkg/hof-0.6.7?dir=hof";
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    , cue-pkg
-    , hof-pkg
-    }:
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-      cue = cue-pkg.defaultPackage.${system};
-      hof = hof-pkg.defaultPackage.${system};
-    in
-    rec {
-      devShell =
-        pkgs.mkShell rec {
-          buildInputs = with pkgs; [
-            defaultPackage
-            cue
-            hof
-          ];
-        };
+  outputs = inputs: inputs.dev.main {
+    inherit inputs;
 
-      defaultPackage =
-        with import nixpkgs { inherit system; };
-        stdenv.mkDerivation rec {
-          name = "${slug}-${version}";
+    config = rec {
+      slug = "c";
+      version_src = ./VERSION;
+      version = builtins.readFile version_src;
+    };
 
-          slug = "c";
-          version = "0.0.2";
+    handler = { pkgs, wrap, system }: rec {
+      devShell = wrap.devShell;
+      defaultPackage = wrap.bashBuilder {
+        src = ./bin;
+        installPhase = ''
+          mkdir -p $out/bin
+          cp -a $src/* $out/bin/
+          chmod 755 $out/bin/*
+        '';
 
-          src = ./bin;
-
-          dontUnpack = true;
-
-          installPhase = ''
-            mkdir -p $out/bin
-            cp -a $src/* $out/bin/
-            chmod 755 $out/bin/c $out/bin/c-*
-          '';
-
-          propagatedBuildInputs = with pkgs; [
-            getopt
-            cue
-            hof
-          ];
-
-          meta = with lib; {
-            homepage = "https://defn.sh/${slug}";
-            description = "common utility: c";
-            platforms = platforms.linux;
-          };
-        };
-    });
+        propagatedBuildInputs = wrap.flakeInputs;
+      };
+    };
+  };
 }
