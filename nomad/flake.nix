5e37d0be4506fd3ead8d3b91d8fa90822a5df6d7
Amanibhavam <iam@defn.sh>
2022-12-01 02:36:46 +0000

    nomad 1.4.3

---
+++ nomad/flake.nix
@@ -0,0 +1,60 @@
+{
+  inputs = {
+    dev.url = github:defn/pkg/dev-0.0.4?dir=dev;
+  };
+
+  outputs = inputs: inputs.dev.main {
+    inherit inputs;
+
+    config = rec {
+      slug = "nomad";
+      version_src = ./VERSION;
+      version = builtins.readFile version_src;
+      vendor_src = ./VENDOR;
+      vendor = builtins.readFile vendor_src;
+
+      url_template = input: "https://releases.hashicorp.com/nomad/${input.version}/nomad_${input.version}_${input.os}_${input.arch}.zip";
+
+      installPhase = { src }: ''
+        install -m 0755 -d $out $out/bin
+        unzip $src
+        install -m 0755 nomad $out/bin/nomad
+      '';
+
+      downloads = {
+        "x86_64-linux" = {
+          version = vendor;
+          os = "linux";
+          arch = "amd64";
+          sha256 = "sha256-FauP0toHHZOFL1m5qIM+OnZJhu+BQMaxH4diE5HmOQI=";
+        };
+        "aarch64-linux" = {
+          version = vendor;
+          os = "linux";
+          arch = "arm64";
+          sha256 = "sha256-IbOvx4vKR4msJxqFXixKmTh20NP4RJlpLQ2dxcir+co=";
+        };
+        "x86_64-darwin" = {
+          version = vendor;
+          os = "darwin";
+          arch = "amd64";
+          sha256 = "sha256-/I11CBNze/UfLVPioWssGKTUZta14xTWVzTbHK1r8gs=";
+        };
+        "aarch64-darwin" = {
+          version = vendor;
+          os = "darwin";
+          arch = "arm64";
+          sha256 = "sha256-TFzVQftil+Yte+iHBx8locdlV44p/eCnhM+n+a56isI=";
+        };
+      };
+    };
+
+    handler = { pkgs, wrap, system }: {
+      devShell = wrap.devShell;
+      defaultPackage = wrap.downloadBuilder {
+        dontUnpack = true;
+        buildInputs = [ pkgs.unzip ];
+      };
+    };
+  };
+}
