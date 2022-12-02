9a182c80830aee64b25ca5da1351453906b5f4a1
Amanibhavam <iam@defn.sh>
2022-12-02 03:07:03 +0000

    incude nomad

--- 48530/flake.nix
+++ 48530/flake.nix
@@ -4,7 +4,8 @@
     elasticsearch.url = github:defn/pkg/elasticsearch-8.5.2?dir=elasticsearch;
     kibana.url = github:defn/pkg/kibana-8.5.2?dir=kibana;
     filebeat.url = github:defn/pkg/filebeat-8.5.2-1?dir=filebeat;
-    caddy.url = github:defn/pkg/caddy-2.6.2?dir=caddy;
+    caddy.url = github:defn/pkg/caddy-2.6.2-1?dir=caddy;
+    nomad.url = github:defn/pkg/nomad-1.4.3?dir=nomad;
   };
 
   outputs = inputs: inputs.dev.main {
