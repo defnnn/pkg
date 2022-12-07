39ed5b72e8e9f982e90b51343d7179925a74145e
Amanibhavam <iam@defn.sh>
2022-12-06 20:00:09 -0800

    acme.sh 3.0.5

---
+++ acme/flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    dev.url = github:defn/pkg/dev-0.0.8?dir=dev;
+  };
+
+  outputs = inputs: inputs.dev.main {
+    inherit inputs;
+
+    config = rec {
+      slug = "acme";
+      version = builtins.readFile ./VERSION;
+      vendor = builtins.readFile ./VENDOR;
+
+      url_template = input: "https://raw.githubusercontent.com/acmesh-official/acme.sh/v${input.version}/acme.sh";
+
+      installPhase = { src }: ''
+        install -m 0755 -d $out $out/bin
+        install -m 0755 $src $out/bin/acme.sh
+      '';
+
+      downloads = {
+        "x86_64-linux" = {
+          version = vendor;
+          sha256 = "sha256-T3a/X9Gc7sSD1KwAFYhJbOEihtquNgrtknd4bhXIVKk=";
+        };
+        "aarch64-linux" = {
+          version = vendor;
+          sha256 = "sha256-T3a/X9Gc7sSD1KwAFYhJbOEihtquNgrtknd4bhXIVKk=";
+        };
+        "x86_64-darwin" = {
+          version = vendor;
+          sha256 = "sha256-T3a/X9Gc7sSD1KwAFYhJbOEihtquNgrtknd4bhXIVKk=";
+        };
+        "aarch64-darwin" = {
+          version = vendor;
+          sha256 = "sha256-T3a/X9Gc7sSD1KwAFYhJbOEihtquNgrtknd4bhXIVKk=";
+        };
+      };
+    };
+
+    handler = { pkgs, wrap, system }: {
+      devShell = wrap.devShell { };
+      defaultPackage = wrap.downloadBuilder { dontUnpack = true; };
+    };
+  };
+}
