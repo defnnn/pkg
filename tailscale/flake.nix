a7e8724274f016c4182e4f21a9fa6db66d7502c7
Amanibhavam <iam@defn.sh>
2022-12-08 17:16:44 -0800

    tailscale 1.34.0

--- tailscale/flake.nix
+++ tailscale/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    dev.url = github:defn/pkg/dev-0.0.4?dir=dev;
+    dev.url = github:defn/pkg/dev-0.0.10-rc2?dir=dev;
   };
 
   outputs = inputs: inputs.dev.main {
@@ -8,57 +8,59 @@
 
     config = rec {
       slug = "tailscale";
-      version_src = ./VERSION;
-      version = builtins.readFile version_src;
-      vendor_src = ./VENDOR;
-      vendor = builtins.readFile vendor_src;
+      version = builtins.readFile ./VERSION;
+      vendor = builtins.readFile ./VENDOR;
 
       url_template = input: "https://pkgs.tailscale.com/stable/tailscale_${input.version}_${input.arch}.tgz";
+
+      downloads = rec {
+        "x86_64-linux" = {
+          version = vendor;
+          arch = "amd64";
+          sha256 = "sha256-QI/Z6SoKCGxinyY8yS2WKJh4MzCk0EEYoWagMOACYRw="; # x86_64-linux
+        };
+        "aarch64-linux" = {
+          version = vendor;
+          arch = "arm64";
+          sha256 = "sha256-EwRebixL8fEoQdrnu0Y0ku3YtQyJpWl/aU9k8kFw+Is="; # aarch64-linux
+        };
+        # these are actually linux, to make nix happy
+        "x86_64-darwin" = {
+          version = vendor;
+          arch = "amd64";
+          sha256 = "sha256-QI/Z6SoKCGxinyY8yS2WKJh4MzCk0EEYoWagMOACYRw="; # x86_64-darwin
+        };
+        "aarch64-darwin" = {
+          version = vendor;
+          arch = "arm64";
+          sha256 = "sha256-EwRebixL8fEoQdrnu0Y0ku3YtQyJpWl/aU9k8kFw+Is="; # aarch64-darwin
+        };
+      };
 
       installPhase = { src }: ''
         install -m 0755 -d $out $out/bin
         install -m 0755 */tailscale $out/bin/tailscale
         install -m 0755 */tailscaled $out/bin/tailscaled
       '';
-
-      downloads = rec {
-        "x86_64-linux" = {
-          version = vendor;
-          arch = "amd64";
-          sha256 = "sha256-0df84XteB2mCtpHWOabyhEzw+tBtTO5suN+4hjIDM0I=";
-        };
-        "aarch64-linux" = {
-          version = vendor;
-          arch = "arm64";
-          sha256 = "sha256-OZLnrM/nl+AxGKDQmj1rd9zpBWsXCGC8ntILxmF4h3w=";
-        };
-        # these are actually linux, to make nix happy
-        "x86_64-darwin" = {
-          version = vendor;
-          arch = "amd64";
-          sha256 = "sha256-0df84XteB2mCtpHWOabyhEzw+tBtTO5suN+4hjIDM0I=";
-        };
-        "aarch64-darwin" = {
-          version = vendor;
-          arch = "arm64";
-          sha256 = "sha256-OZLnrM/nl+AxGKDQmj1rd9zpBWsXCGC8ntILxmF4h3w=";
-        };
-      };
     };
 
-    handler = { pkgs, wrap, system }: rec {
-      devShell = wrap.devShell;
-      defaultPackage = wrap.downloadBuilder { };
+    handler = { pkgs, wrap, system }:
+      let
+        commonBuild = { };
+        builds = wrap.genDownloadBuilders commonBuild;
+      in
+      builds // {
+        devShell = wrap.devShell { };
 
-      apps.default = {
-        type = "app";
-        program = "${defaultPackage}/bin/tailscale";
-      };
+        apps.default = {
+          type = "app";
+          program = "${builds.defaultPackage}/bin/tailscale";
+        };
 
-      apps.tailscaled = {
-        type = "app";
-        program = "${defaultPackage}/bin/tailscaled";
+        apps.tailscaled = {
+          type = "app";
+          program = "${builds.defaultPackage}/bin/tailscaled";
+        };
       };
-    };
   };
 }
