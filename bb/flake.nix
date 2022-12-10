5095421bd693dd8c17d550f2cfb0816d23c398d7
Amanibhavam <iam@defn.sh>
2022-12-10 15:45:48 -0800

    bb 1.0.168

---
+++ bb/flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs = {
+    dev.url = github:defn/pkg/dev-0.0.10?dir=dev;
+  };
+
+  outputs = inputs: inputs.dev.main {
+    inherit inputs;
+
+    config = rec {
+      slug = "bb";
+      version = builtins.readFile ./VERSION;
+      vendor = builtins.readFile ./VENDOR;
+
+      url_template = input: "https://github.com/babashka/babashka/releases/download/v${input.version}/babashka-${input.version}-${input.os}-${input.arch}.tar.gz";
+
+      downloads = {
+        "x86_64-linux" = {
+          version = vendor;
+          os = "linux";
+          arch = "amd64";
+          sha256 = "sha256-RL2bmphXdASj0DkctcqEYRa/DjuhuGbiDgA46WE46Iw="; # x86_64-linux
+        };
+        "aarch64-linux" = {
+          version = vendor;
+          os = "linux";
+          arch = "aarch64";
+          sha256 = "sha256-KZua9FY0jWgFpqaBp9FrWlUrRaXcT6BeHGvkqRI9ato="; # aarch64-linux
+        };
+        "x86_64-darwin" = {
+          version = vendor;
+          os = "macos";
+          arch = "amd64";
+          sha256 = "sha256-N5pPULowLTxaXiaH+eHf4CrL2ah+C0YZ9PB5EybzQNA="; # x86_64-darwin
+        };
+        "aarch64-darwin" = {
+          version = vendor;
+          os = "macos";
+          arch = "aarch64";
+          sha256 = "sha256-EcS0vQtTTbHs1zKwO8N2+LIbvaDYjKy0u+FbhGkCkSM="; # aarch64-darwin
+        };
+      };
+
+      installPhase = { src }: ''
+        install -m 0755 -d $out $out/bin
+        ls -ltrhd *
+        install -m 0755 bb $out/bin/bb
+      '';
+    };
+
+    handler = { pkgs, wrap, system }:
+      wrap.genDownloadBuilders { };
+  };
+}
