adb066d2e489ea72fdc70a8819541b153ff5f235
Amanibhavam <iam@defn.sh>
2022-12-18 15:19:14 -0800

    use latest hof, cue

--- c/flake.nix
+++ c/flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
     dev.url = github:defn/pkg/dev-0.0.16?dir=dev;
-    cue.url = "github:defn/pkg/cue-0.4.3-1?dir=cue";
-    hof.url = "github:defn/pkg/hof-0.6.7-2?dir=hof";
+    cue.url = "github:defn/pkg/cue-0.4.3-2?dir=cue";
+    hof.url = "github:defn/pkg/hof-0.6.7-3?dir=hof";
   };
 
   outputs = inputs: inputs.dev.main rec {
