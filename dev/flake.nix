fdf2e168283f0c574684de50c12f70355f5624da
amanibhavam <iam@defn.sh>
2023-01-05 01:16:41 +0000

    dev: wrapper generates system packages again

--- dev/flake.nix
+++ dev/flake.nix
@@ -4,7 +4,7 @@
     flake-utils.url = github:numtide/flake-utils?rev=04c1b180862888302ddfb2e3ad9eaa63afc60cf8;
 
     wrapper = {
-      url = github:defn/pkg/wrapper-0.0.14?dir=wrapper;
+      url = github:defn/pkg/wrapper-0.0.15?dir=wrapper;
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
