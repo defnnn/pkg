952c3ac6f90245fb74eda87ea8812d176c4077f5
amanibhavam <iam@defn.sh>
2023-01-10 20:56:58 -0800

    acme: install dns helpers

--- acme/flake.nix
+++ acme/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    dev.url = github:defn/pkg/dev-0.0.17?dir=dev;
+    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
   };
 
   outputs = inputs: inputs.dev.main rec {
@@ -10,44 +10,49 @@
 
     config = rec {
       slug = builtins.readFile ./SLUG;
-      version = builtins.readFile ./VERSION;
       vendor = builtins.readFile ./VENDOR;
+      version ="${slug}-${vendor}";
 
-      url_template = input: "https://raw.githubusercontent.com/acmesh-official/acme.sh/v${input.version}/acme.sh";
+      url_template = input: "https://github.com/acmesh-official/acme.sh/archive/refs/tags/v${input.version}.tar.gz";
 
       downloads = {
         "x86_64-linux" = {
           version = vendor;
-          sha256 = "sha256-T3a/X9Gc7sSD1KwAFYhJbOEihtquNgrtknd4bhXIVKk="; # x86_64-linux
+          sha256 = "sha256-oZEy7WVJFAmo2Kk8Jk3LzyZakdRaRt5nSBG5P1mVOY8"; # x86_64-linux
         };
         "aarch64-linux" = {
           version = vendor;
-          sha256 = "sha256-T3a/X9Gc7sSD1KwAFYhJbOEihtquNgrtknd4bhXIVKk="; # aarch64-linux
+          sha256 = "sha256-oZEy7WVJFAmo2Kk8Jk3LzyZakdRaRt5nSBG5P1mVOY8"; # aarch64-linux
         };
         "x86_64-darwin" = {
           version = vendor;
-          sha256 = "sha256-T3a/X9Gc7sSD1KwAFYhJbOEihtquNgrtknd4bhXIVKk="; # x86_64-darwin
+          sha256 = "sha256-oZEy7WVJFAmo2Kk8Jk3LzyZakdRaRt5nSBG5P1mVOY8"; # x86_64-darwin
         };
         "aarch64-darwin" = {
           version = vendor;
-          sha256 = "sha256-T3a/X9Gc7sSD1KwAFYhJbOEihtquNgrtknd4bhXIVKk="; # aarch64-darwin
+          sha256 = "sha256-oZEy7WVJFAmo2Kk8Jk3LzyZakdRaRt5nSBG5P1mVOY8"; # aarch64-darwin
         };
       };
 
       installPhase = { src }: ''
-        install -m 0755 -d $out $out/bin
-        install -m 0755 $src $out/bin/acme.sh
+        install -m 0755 -d $out $out/bin $out/bin/dnsapi
+        install -m 0755 */acme.sh $out/bin/acme.sh
+        cp */dnsapi/* $out/bin/dnsapi/
       '';
     };
 
     handler = { pkgs, wrap, system, builders }:
       let
-        builds = wrap.genDownloadBuilders { dontUnpack = true; };
+        builds = wrap.genDownloadBuilders { };
       in
       builds // {
         apps.default = {
           type = "app";
           program = "${builds.defaultPackage}/bin/acme.sh";
+        };
+      } // {
+        defaultPackage = builds.defaultPackage // {
+          sourceRoot = ".";
         };
       };
   };
