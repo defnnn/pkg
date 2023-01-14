12d27d0a2daadeeb5c6f17acfeeb4bbef83e8a0c
amanibhavam <iam@defn.sh>
2023-01-13 18:41:48 -0800

    flyctl 0.0.450

--- flyctl/flake.nix
+++ flyctl/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    dev.url = github:defn/pkg/dev-0.0.19?dir=dev;
+    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
   };
 
   outputs = inputs: inputs.dev.main rec {
@@ -21,25 +21,25 @@
           version = vendor;
           os = "Linux";
           arch = "x86_64";
-          sha256 = "sha256-7T0B+A4S4MCyqdg1Bhuvvo1GwhpoIVr/Pzw98mMosT8="; # x86_64-linux
+          sha256 = "sha256-aaaB+A4S4MCyqdg1Bhuvvo1GwhpoIVr/Pzw98mMosT8="; # x86_64-linux
         };
         "aarch64-linux" = {
           version = vendor;
           os = "Linux";
           arch = "arm64";
-          sha256 = "sha256-R0aTsHj4lKeq0OTAL2Ke5Nf+yM5dHlZ07Rkj+va5pvg="; # aarch64-linux
+          sha256 = "sha256-aaaTsHj4lKeq0OTAL2Ke5Nf+yM5dHlZ07Rkj+va5pvg="; # aarch64-linux
         };
         "x86_64-darwin" = {
           version = vendor;
           os = "macOS";
           arch = "x86_64";
-          sha256 = "sha256-6oIbB7iZB+onds/UUTppW89kQpdVVTOXvQp7tg07eBY="; # x86_64-darwin
+          sha256 = "sha256-9HlEqUfZ7tjPt+m+/GhM3mVydku5ZhUvDKk7J7HdKI0="; # x86_64-darwin
         };
         "aarch64-darwin" = {
           version = vendor;
           os = "macOS";
           arch = "arm64";
-          sha256 = "sha256-4tLM1cRG5wzqf5nJot3basg2hviad0LoDRBBpZxIaZQ="; # aarch64-darwin
+          sha256 = "sha256-9BVCzg58AGvuvWK7r0dT9WfiuA/zh1+eb6qOmI+p9Dg="; # aarch64-darwin
         };
       };
 
