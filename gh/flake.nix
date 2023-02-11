0d9088c86db43cf9ca0d7b55fbdb32991b4d47dd
amanibhavam <iam@defn.sh>
2023-02-11 04:25:07 +0000

    gh 2.23.0

--- gh/flake.nix
+++ gh/flake.nix
@@ -6,7 +6,7 @@
   outputs = inputs: inputs.dev.main rec {
     inherit inputs;
 
-    src = builtins.path { path = ./.; name = config.slug; };
+    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
 
     config = rec {
       slug = builtins.readFile ./SLUG;
@@ -21,25 +21,25 @@
           version = vendor;
           os = "linux";
           arch = "amd64";
-          sha256 = "sha256-dvfhi9rV3f38q0D86GyNb5+yf50pwSh833Hg1rRbqEs="; # x86_64-linux
+          sha256 = "sha256-d0xKsW6WL4UWZBYuTV6+b+KnuBQ6+vGi67/RFfBRfaA="; # x86_64-linux
         };
         "aarch64-linux" = {
           version = vendor;
           os = "linux";
           arch = "arm64";
-          sha256 = "sha256-kSICTan+R+FccY29ilfAf5dQ5jEq2RtAXYNbDdRPdG0="; # aarch64-linux
+          sha256 = "sha256-LzthWie/SVQCzWElJuurEj0LDrcCYkR+wB41fYmYHTk="; # aarch64-linux
         };
         "x86_64-darwin" = {
           version = vendor;
           os = "macos";
           arch = "amd64";
-          sha256 = "sha256-qa9Do+VeRGA4jUbDcqnpTe9nwXrnAK8R5QhN6jCEbUE="; # x86_64-darwin
+          sha256 = "sha256-sIB9Xw2vz6j6LJEb62PPP/vGlYooL8yYVF3J2QcP5LY="; # x86_64-darwin
         };
         "aarch64-darwin" = {
           version = vendor;
           os = "macos";
           arch = "amd64"; # no arm64 macos
-          sha256 = "sha256-qa9Do+VeRGA4jUbDcqnpTe9nwXrnAK8R5QhN6jCEbUE="; # aarch64-darwin
+          sha256 = "sha256-sIB9Xw2vz6j6LJEb62PPP/vGlYooL8yYVF3J2QcP5LY="; # aarch64-darwin
         };
       };
 
