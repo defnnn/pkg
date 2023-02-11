4791cb13b05d66801ea1fa2d312ed01f941e1f51
amanibhavam <iam@defn.sh>
2023-02-11 04:21:42 +0000

    tilt 0.31.2

--- tilt/flake.nix
+++ tilt/flake.nix
@@ -6,39 +6,40 @@
   outputs = inputs: inputs.dev.main rec {
     inherit inputs;
 
-    src = builtins.path { path = ./.; name = config.slug; };
+    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
 
     config = rec {
       slug = builtins.readFile ./SLUG;
-      revision = builtins.readFile ./REVISION;
       vendor = builtins.readFile ./VENDOR;
-      version = "${slug}-${vendor}";
+      revision = builtins.readFile ./REVISION;
+      version = "${vendor}-${revision}";
 
       url_template = input: "https://github.com/tilt-dev/tilt/releases/download/v${input.version}/tilt.${input.version}.${input.os}.${input.arch}.tar.gz";
+
       downloads = {
         "x86_64-linux" = {
           version = vendor;
           os = "linux";
           arch = "x86_64";
-          sha256 = "sha256-DzmxJbqJ7V1r03ByyhYQBoxujMCKfyJqAOdV7X9X/w4="; # x86_64-linux
+          sha256 = "sha256-nsGiGTkzZ/xB3V9fEIP2DW6YOeWCG4FwwmSBv2sZ3H4="; # x86_64-linux
         };
         "aarch64-linux" = {
           version = vendor;
           os = "linux";
           arch = "arm64";
-          sha256 = "sha256-FTWJHxKGE7ln9S+mNeBiCyDLKoScb6llWie72vkF4go="; # aarch64-linux
+          sha256 = "sha256-ig/KOynkgr08WPwXd+S+BfUIcjpkgklIeKJ199Wn8sE="; # aarch64-linux
         };
         "x86_64-darwin" = {
           version = vendor;
           os = "mac";
           arch = "x86_64";
-          sha256 = "sha256-/KXvW7kIb/l0oGG84bSEJo7nJQiigM2xfmShwdd7WQE="; # x86_64-darwin
+          sha256 = "sha256-reCHfnL1agiTd+UX78hjaSIKx7aoHweOjpmpwpqiROs="; # x86_64-darwin
         };
         "aarch64-darwin" = {
           version = vendor;
           os = "mac";
           arch = "arm64";
-          sha256 = "sha256-Uh5RVfZP2oRMxJFuhdpONEPVNLG0uUNSP9iwI38IJ+0="; # aarch64-darwin
+          sha256 = "sha256-2h1bSzspBY1682izyTWksdsrHyXavNlfwcQzA65zZf0="; # aarch64-darwin
         };
       };
 
