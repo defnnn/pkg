f96529fda70023df70a1c18153a9445b084e2461
amanibhavam <iam@defn.sh>
2023-02-11 03:48:48 +0000

    kubernetes tools

---
+++ kubernetes/flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
+    helm.url = github:defn/pkg/helm-3.11.1-0?dir=helm;
+    kustomize.url = github:defn/pkg/kustomize-5.0.0-0?dir=kustomize;
+    argo.url = github:defn/pkg/argo-3.4.5-0?dir=argo;
+    argocd.url = github:defn/pkg/argocd-2.6.1-0?dir=argocd;
+    argorollouts.url = github:defn/pkg/argorollouts-1.4.0-1?dir=argorollouts;
+  };
+
+  outputs = inputs: inputs.dev.main rec {
+    inherit inputs;
+
+    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
+
+    config = rec {
+      slug = builtins.readFile ./SLUG;
+      version = builtins.readFile ./VERSION;
+    };
+
+    handler = { pkgs, wrap, system, builders }: rec {
+      defaultPackage = wrap.nullBuilder {
+        propagatedBuildInputs = wrap.flakeInputs;
+      };
+    };
+  };
+}
