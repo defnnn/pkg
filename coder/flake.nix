fb04ca58fb1fd825560c8dd02bd477c95eb8fdd3
amanibhavam <iam@defn.sh>
2023-02-11 17:50:15 +0000

    coder 0.17.1

--- coder/flake.nix
+++ coder/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    dev.url = github:defn/pkg/dev-0.0.18?dir=dev;
+    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
   };
 
   outputs = inputs: inputs.dev.main rec {
@@ -12,7 +12,7 @@
       slug = builtins.readFile ./SLUG;
       vendor = builtins.readFile ./VENDOR;
       revision = builtins.readFile ./REVISION;
-      version = "${slug}-${vendor}-${revision}";
+      version = "${vendor}-${revision}";
 
       url_template = input: "${input.url}";
 
@@ -21,28 +21,28 @@
           version = vendor;
           os = "linux";
           arch = "amd64";
-          sha256 = "sha256-/dqLI+ZiBRIIShfUxelyO0lZc0XpFCt+USUoOoGduL4="; # x86_64-linux
+          sha256 = "sha256-aQ9s9RGsu2Nyx+iZuns5wXqABcQlpL8YqW553gVrTe0="; # x86_64-linux
           url = "https://github.com/coder/coder/releases/download/v${version}/coder_${version}_${os}_${arch}.tar.gz";
         };
         "aarch64-linux" = rec {
           version = vendor;
           os = "linux";
           arch = "arm64";
-          sha256 = "sha256-zZ/uozGWuw3Cuwwf06/3IIWYnx6rf5RD5D1Z7Gid5L0="; # aarch64-linux
+          sha256 = "sha256-of4m469IWpEObqdWBb/1nuI0GlhyrABvs96LKyLqDF4="; # aarch64-linux
           url = "https://github.com/coder/coder/releases/download/v${version}/coder_${version}_${os}_${arch}.tar.gz";
         };
         "x86_64-darwin" = rec {
           version = vendor;
           os = "darwin";
           arch = "amd64";
-          sha256 = "sha256-ImOD+xp1GokoJUGPs+G9z90jLbwbYmKOxBKnsmLJ/UM="; # x86_64-darwin
+          sha256 = "sha256-YfwcN4hY0PpIzLTBLcT0M+XpFi5RibKztSOxMVrRYAo="; # x86_64-darwin
           url = "https://github.com/coder/coder/releases/download/v${version}/coder_${version}_${os}_${arch}.zip";
         };
         "aarch64-darwin" = rec {
           version = vendor;
           os = "darwin";
           arch = "arm64";
-          sha256 = "sha256-xV5F0Kwxz10Mxr6c0dmyShppzq8gDii8SxuW5pz1xC8="; # aarch64-darwin
+          sha256 = "sha256-OhE11DpVfgjY0lbeCbA0+MxXBT+GaGaDj4RpY6ujNfQ="; # aarch64-darwin
           url = "https://github.com/coder/coder/releases/download/v${version}/coder_${version}_${os}_${arch}.zip";
         };
       };
