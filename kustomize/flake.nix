5352205f534d260bf9abdb090b37ddca5d0ddd49
amanibhavam <iam@defn.sh>
2023-02-11 03:19:07 +0000

    kustomize 5.0.0

--- kustomize/flake.nix
+++ kustomize/flake.nix
@@ -1,17 +1,18 @@
 {
   inputs = {
-    dev.url = github:defn/pkg/dev-0.0.17?dir=dev;
+    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
   };
 
   outputs = inputs: inputs.dev.main rec {
     inherit inputs;
 
-    src = builtins.path { path = ./.; name = config.slug; };
+    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
 
     config = rec {
       slug = builtins.readFile ./SLUG;
-      version = builtins.readFile ./VERSION;
       vendor = builtins.readFile ./VENDOR;
+      revision = builtins.readFile ./REVISION;
+      version = "${vendor}-${revision}";
 
       url_template = input: "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${input.version}/kustomize_v${input.version}_${input.os}_${input.arch}.tar.gz";
 
@@ -20,25 +21,25 @@
           version = vendor;
           os = "linux";
           arch = "amd64";
-          sha256 = "sha256-cB48S/oU5MUg1IH99xMfkCUxv8ACy1Bi3PMSY6CccMk"; # x86_64-linux
+          sha256 = "sha256-LowoqAziE1KCUfSJ240ty+p8Y7mGyPdZWjn8dv+HHNc="; # x86_64-linux
         };
         "aarch64-linux" = {
           version = vendor;
           os = "linux";
           arch = "arm64";
-          sha256 = "sha256-ZWZbOSl8xzwTkY8Fu+hFDRdVbwrNFiQqM5Jx4Uhh32c"; # aarch64-linux
+          sha256 = "sha256-6XsSqD57mwQHysl8rEwlvBNcQjg703ZNVUTjLJZULso="; # aarch64-linux
         };
         "x86_64-darwin" = {
           version = vendor;
           os = "darwin";
           arch = "amd64";
-          sha256 = "sha256-b9V+eO0MBrW92CdQxdxtD5kqe5JtEU/pS+Rten4ytjo"; # x86_64-darwin
+          sha256 = "sha256-db0Od2oeHERjmqAXu6m2owXOczK4m56Aiemf7iuD0Eo="; # x86_64-darwin
         };
         "aarch64-darwin" = {
           version = vendor;
           os = "darwin";
           arch = "arm64";
-          sha256 = "sha256-PB6Llc70/25S1fS4xluNnQa3X0LRy0CYbB1ncp2CQRo"; # aarch64-darwin
+          sha256 = "sha256-dMV2qdbenWq7PohhQWNbgejPPCMxsBFTXU6LURnykds="; # aarch64-darwin
         };
       };
 
