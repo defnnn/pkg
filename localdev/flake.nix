c3e157b299915a29776396f0f3ffb7616fe2dec6
amanibhavam <iam@defn.sh>
2023-02-14 18:59:38 +0000

    localdev: bump coder, codeserver

--- localdev/flake.nix
+++ localdev/flake.nix
@@ -3,8 +3,8 @@
     dev.url = github:defn/pkg/dev-0.0.23?dir=dev;
     latest.url = github:NixOS/nixpkgs?rev=4938e72add339f76d795284cb5a3aae85d02ee53;
     caddy.url = github:defn/pkg/caddy-2.6.3-0?dir=caddy;
-    coder.url = github:defn/pkg/coder-0.17.2-0?dir=coder;
-    codeserver.url = github:defn/pkg/codeserver-4.10.0-rc.1-0?dir=codeserver;
+    coder.url = github:defn/pkg/coder-0.17.3-0?dir=coder;
+    codeserver.url = github:defn/pkg/codeserver-4.10.0-rc.2-0?dir=codeserver;
     terraform.url = github:defn/pkg/terraform-1.3.4?dir=terraform;
     earthly.url = github:defn/pkg/earthly-0.7.0-rc2-0?dir=earthly;
     tilt.url = github:defn/pkg/tilt-0.31.2-0?dir=tilt;
