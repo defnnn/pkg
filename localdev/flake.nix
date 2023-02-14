a3de0042f27b7d60734b248db5a0a38dff42aed6
amanibhavam <iam@defn.sh>
2023-02-14 00:36:57 +0000

    localdev: code-server 4.10.0-rc1

--- localdev/flake.nix
+++ localdev/flake.nix
@@ -4,7 +4,7 @@
     latest.url = github:NixOS/nixpkgs?rev=4938e72add339f76d795284cb5a3aae85d02ee53;
     caddy.url = github:defn/pkg/caddy-2.6.3-0?dir=caddy;
     coder.url = github:defn/pkg/coder-0.17.1-0?dir=coder;
-    codeserver.url = github:defn/pkg/codeserver-4.10.0-rc.0-0?dir=codeserver;
+    codeserver.url = github:defn/pkg/codeserver-4.10.0-rc.1-0?dir=codeserver;
     terraform.url = github:defn/pkg/terraform-1.3.4?dir=terraform;
     earthly.url = github:defn/pkg/earthly-0.7.0-rc2-0?dir=earthly;
     tilt.url = github:defn/pkg/tilt-0.31.2-0?dir=tilt;
