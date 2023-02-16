1ece11d00df87355fdae730b4fbb2bffde61f5fa
amanibhavam <iam@defn.sh>
2023-02-16 05:25:38 +0000

    k9s: latest

--- k9s/flake.nix
+++ k9s/flake.nix
@@ -1,55 +1,36 @@
 {
-  inputs = {
-    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
-  };
+  inputs.pkg.url = github:defn/pkg/0.0.139;
+  outputs = inputs: inputs.pkg.downloadMain {
+    src = ./.;
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
+    url_template = input: "https://github.com/derailed/k9s/releases/download/v${input.vendor}/k9s_${input.os}_${input.arch}.tar.gz";
 
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
+    installPhase = { src }: ''
+      install -m 0755 -d $out $out/bin
+      install -m 0755 k9s $out/bin/k9s
+    '';
 
-    config = rec {
-      slug = builtins.readFile ./SLUG;
-      vendor = builtins.readFile ./VENDOR;
-      revision = builtins.readFile ./REVISION;
-      version = "${vendor}-${revision}";
-
-      url_template = input: "https://github.com/derailed/k9s/releases/download/v${input.version}/k9s_${input.os}_${input.arch}.tar.gz";
-
-      downloads = {
-        "x86_64-linux" = {
-          version = vendor;
-          os = "Linux";
-          arch = "amd64";
-          sha256 = "sha256-jUv7m/7o4oMAMFdGACACsn9HutvRniWlA9OBcgBuEXA="; # x86_64-linux
-        };
-        "aarch64-linux" = {
-          version = vendor;
-          os = "Linux";
-          arch = "arm64";
-          sha256 = "sha256-50UWYnfC6rfh0G/MNJLQHvpqWmvQx3oGj0eknU2OcnI="; # aarch64-linux
-        };
-        "x86_64-darwin" = {
-          version = vendor;
-          os = "Darwin";
-          arch = "amd64";
-          sha256 = "sha256-qsMRgl99kPIOsnadfaKNP6Wc60hlLCYV3WMPd7Qe/5g="; # x86_64-darwin
-        };
-        "aarch64-darwin" = {
-          version = vendor;
-          os = "Darwin";
-          arch = "arm64";
-          sha256 = "sha256-E+cbY9pvGYdY/8WEAZHXpNnxFkV+TW8G1gFkrkNvbGQ="; # aarch64-darwin
-        };
+    downloads = {
+      "x86_64-linux" = {
+        os = "Linux";
+        arch = "amd64";
+        sha256 = "sha256-jUv7m/7o4oMAMFdGACACsn9HutvRniWlA9OBcgBuEXA="; # x86_64-linux
       };
-
-      installPhase = { src }: ''
-        install -m 0755 -d $out $out/bin
-        install -m 0755 k9s $out/bin/k9s
-      '';
+      "aarch64-linux" = {
+        os = "Linux";
+        arch = "arm64";
+        sha256 = "sha256-50UWYnfC6rfh0G/MNJLQHvpqWmvQx3oGj0eknU2OcnI="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "Darwin";
+        arch = "amd64";
+        sha256 = "sha256-qsMRgl99kPIOsnadfaKNP6Wc60hlLCYV3WMPd7Qe/5g="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "Darwin";
+        arch = "arm64";
+        sha256 = "sha256-E+cbY9pvGYdY/8WEAZHXpNnxFkV+TW8G1gFkrkNvbGQ="; # aarch64-darwin
+      };
     };
-
-    handler = { pkgs, wrap, system, builders }:
-      wrap.genDownloadBuilders { };
   };
 }
