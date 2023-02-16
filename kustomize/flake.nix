9d569d50e501e6c1e6730994b43612500f89c227
amanibhavam <iam@defn.sh>
2023-02-16 05:21:53 +0000

    kustomize: latest

--- kustomize/flake.nix
+++ kustomize/flake.nix
@@ -1,36 +1,35 @@
 {
-  inputs.pkg.url = github:defn/pkg/0.0.133;
+  inputs.pkg.url = github:defn/pkg/0.0.139;
   outputs = inputs: inputs.pkg.downloadMain {
     src = ./.;
-    config = rec {
-      url_template = input: "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${input.vendor}/kustomize_v${input.vendor}_${input.os}_${input.arch}.tar.gz";
 
-      installPhase = { src }: ''
-        install -m 0755 -d $out $out/bin
-        install -m 0755 kustomize $out/bin/kustomize
-      '';
+    url_template = input: "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${input.vendor}/kustomize_v${input.vendor}_${input.os}_${input.arch}.tar.gz";
 
-      downloads = {
-        "x86_64-linux" = {
-          os = "linux";
-          arch = "amd64";
-          sha256 = "sha256-LowoqAziE1KCUfSJ240ty+p8Y7mGyPdZWjn8dv+HHNc="; # x86_64-linux
-        };
-        "aarch64-linux" = {
-          os = "linux";
-          arch = "arm64";
-          sha256 = "sha256-6XsSqD57mwQHysl8rEwlvBNcQjg703ZNVUTjLJZULso="; # aarch64-linux
-        };
-        "x86_64-darwin" = {
-          os = "darwin";
-          arch = "amd64";
-          sha256 = "sha256-db0Od2oeHERjmqAXu6m2owXOczK4m56Aiemf7iuD0Eo="; # x86_64-darwin
-        };
-        "aarch64-darwin" = {
-          os = "darwin";
-          arch = "arm64";
-          sha256 = "sha256-dMV2qdbenWq7PohhQWNbgejPPCMxsBFTXU6LURnykds="; # aarch64-darwin
-        };
+    installPhase = { src }: ''
+      install -m 0755 -d $out $out/bin
+      install -m 0755 kustomize $out/bin/kustomize
+    '';
+
+    downloads = {
+      "x86_64-linux" = {
+        os = "linux";
+        arch = "amd64";
+        sha256 = "sha256-LowoqAziE1KCUfSJ240ty+p8Y7mGyPdZWjn8dv+HHNc="; # x86_64-linux
+      };
+      "aarch64-linux" = {
+        os = "linux";
+        arch = "arm64";
+        sha256 = "sha256-6XsSqD57mwQHysl8rEwlvBNcQjg703ZNVUTjLJZULso="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "darwin";
+        arch = "amd64";
+        sha256 = "sha256-db0Od2oeHERjmqAXu6m2owXOczK4m56Aiemf7iuD0Eo="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "darwin";
+        arch = "arm64";
+        sha256 = "sha256-dMV2qdbenWq7PohhQWNbgejPPCMxsBFTXU6LURnykds="; # aarch64-darwin
       };
     };
   };
