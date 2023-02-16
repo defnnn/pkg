15ba04bc960864f3273d33a4402fc0e24d9f927c
amanibhavam <iam@defn.sh>
2023-02-16 02:13:05 +0000

    export main

--- flake.nix
+++ flake.nix
@@ -3,15 +3,29 @@
     dev.url = github:defn/pkg/dev-0.0.23?dir=dev;
   };
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
+  outputs = inputs:
+    let
+      main = caller: inputs.pkg.main rec {
+        inherit inputs;
 
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
+        src = builtins.path { path = caller.src; name = (builtins.fromJSON (builtins.readFile "${caller.src}/flake.json")).slug; };
 
-    handler = { pkgs, wrap, system, builders, commands, config }: {
-      defaultPackage = wrap.nullBuilder {
-        propagatedBuildInputs = with pkgs; [ cachix ];
+        handler = { pkgs, wrap, system, builders, commands, config }@ctx: rec {
+          defaultPackage = caller.defaultPackage ctx;
+        };
+      };
+    in
+    {
+      inherit main;
+    } // inputs.dev.main rec {
+      inherit inputs;
+
+      src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
+
+      handler = { pkgs, wrap, system, builders, commands, config }: {
+        defaultPackage = wrap.nullBuilder {
+          propagatedBuildInputs = with pkgs; [ cachix ];
+        };
       };
     };
-  };
 }
