594917fb5834b1f99b12bcd26cc0dbdf4c94c3cf
amanibhavam <iam@defn.sh>
2023-02-16 05:58:02 +0000

    argocd: latest

--- argocd/flake.nix
+++ argocd/flake.nix
@@ -1,55 +1,40 @@
 {
-  inputs = {
-    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
-  };
+  inputs.pkg.url = github:defn/pkg/0.0.140;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
+    url_template = input: "https://github.com/argoproj/argo-cd/releases/download/v${input.vendor}/argocd-${input.os}-${input.arch}";
 
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
+    installPhase = { src }: ''
+      install -m 0755 -d $out $out/bin
+      install -m 0755 $src $out/bin/argocd
+    '';
 
-    config = rec {
-      slug = builtins.readFile ./SLUG;
-      vendor = builtins.readFile ./VENDOR;
-      revision = builtins.readFile ./REVISION;
-      version = "${vendor}-${revision}";
-
-      url_template = input: "https://github.com/argoproj/argo-cd/releases/download/v${input.version}/argocd-${input.os}-${input.arch}";
-
-      downloads = {
-        "x86_64-linux" = {
-          version = vendor;
-          os = "linux";
-          arch = "amd64";
-          sha256 = "sha256-gcH4kh8YxWCXraaXxKL1W4RUjlNvV2mN/Y3UmjF1xrY="; # x86_64-linux
-        };
-        "aarch64-linux" = {
-          version = vendor;
-          os = "linux";
-          arch = "arm64";
-          sha256 = "sha256-Pj/G34fO1++2MhIRSS0GFJahbJLBs59mSJMGU3c28G4="; # aarch64-linux
-        };
-        "x86_64-darwin" = {
-          version = vendor;
-          os = "darwin";
-          arch = "amd64";
-          sha256 = "sha256-YeMdLS8qyxQQJ8iYEaOGPyf7s+f5hLyZwjWSvLBKims="; # x86_64-darwin
-        };
-        "aarch64-darwin" = {
-          version = vendor;
-          os = "darwin";
-          arch = "arm64";
-          sha256 = "sha256-+9J+Vn+XCHXNjjnEoVOVgGiJXyf/eh9rvZ+lR1UgVJU="; # aarch64-darwin
-        };
+    downloads = {
+      options = {
+        dontUnpack = true;
       };
 
-      installPhase = { src }: ''
-        install -m 0755 -d $out $out/bin
-        install -m 0755 $src $out/bin/argocd
-      '';
+      "x86_64-linux" = {
+        os = "linux";
+        arch = "amd64";
+        sha256 = "sha256-gcH4kh8YxWCXraaXxKL1W4RUjlNvV2mN/Y3UmjF1xrY="; # x86_64-linux
+      };
+      "aarch64-linux" = {
+        os = "linux";
+        arch = "arm64";
+        sha256 = "sha256-Pj/G34fO1++2MhIRSS0GFJahbJLBs59mSJMGU3c28G4="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "darwin";
+        arch = "amd64";
+        sha256 = "sha256-YeMdLS8qyxQQJ8iYEaOGPyf7s+f5hLyZwjWSvLBKims="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "darwin";
+        arch = "arm64";
+        sha256 = "sha256-+9J+Vn+XCHXNjjnEoVOVgGiJXyf/eh9rvZ+lR1UgVJU="; # aarch64-darwin
+      };
     };
-
-    handler = { pkgs, wrap, system, builders }:
-      wrap.genDownloadBuilders { dontUnpack = true; };
   };
 }
