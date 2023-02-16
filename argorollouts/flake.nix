ca405e21e5fa8600b62427bd6613f0298813f7a0
amanibhavam <iam@defn.sh>
2023-02-16 06:27:32 +0000

    argorollouts: latest

--- argorollouts/flake.nix
+++ argorollouts/flake.nix
@@ -1,55 +1,40 @@
 {
-  inputs = {
-    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
-  };
+  inputs.pkg.url = github:defn/pkg/0.0.141;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
+    url_template = input: "https://github.com/argoproj/argo-rollouts/releases/download/v${input.vendor}/kubectl-argo-rollouts-${input.os}-${input.arch}";
 
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
+    installPhase = { src }: ''
+      install -m 0755 -d $out $out/bin
+      install -m 0755 $src $out/bin/argo-rollouts
+    '';
 
-    config = rec {
-      slug = builtins.readFile ./SLUG;
-      vendor = builtins.readFile ./VENDOR;
-      revision = builtins.readFile ./REVISION;
-      version = "${vendor}-${revision}";
-
-      url_template = input: "https://github.com/argoproj/argo-rollouts/releases/download/v${input.version}/kubectl-argo-rollouts-${input.os}-${input.arch}";
-
-      downloads = {
-        "x86_64-linux" = {
-          version = vendor;
-          os = "linux";
-          arch = "amd64";
-          sha256 = "sha256-IJarUoOjGmqYTOWbmW6m2YyCRv6R3Np+v0IGMqVQHc4="; # x86_64-linux
-        };
-        "aarch64-linux" = {
-          version = vendor;
-          os = "linux";
-          arch = "arm64";
-          sha256 = "sha256-I1VrKmi028NZ7EMKifwuptqKO1/JzBYmGkojU/aW39g="; # aarch64-linux
-        };
-        "x86_64-darwin" = {
-          version = vendor;
-          os = "darwin";
-          arch = "amd64";
-          sha256 = "sha256-VFM+tnptNV98OywqnfmcFgYtqep8wgHBpGTcROwxda0="; # x86_64-darwin
-        };
-        "aarch64-darwin" = {
-          version = vendor;
-          os = "darwin";
-          arch = "arm64";
-          sha256 = "sha256-egv4BKxRI6rfGnAiLjBlfmc5heyXUtP2LDbI5B4t/lo="; # aarch64-darwin
-        };
+    downloads = {
+      options = {
+        dontUnpack = true;
       };
 
-      installPhase = { src }: ''
-        install -m 0755 -d $out $out/bin
-        install -m 0755 $src $out/bin/argo-rollouts
-      '';
+      "x86_64-linux" = {
+        os = "linux";
+        arch = "amd64";
+        sha256 = "sha256-IJarUoOjGmqYTOWbmW6m2YyCRv6R3Np+v0IGMqVQHc4="; # x86_64-linux
+      };
+      "aarch64-linux" = {
+        os = "linux";
+        arch = "arm64";
+        sha256 = "sha256-I1VrKmi028NZ7EMKifwuptqKO1/JzBYmGkojU/aW39g="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "darwin";
+        arch = "amd64";
+        sha256 = "sha256-VFM+tnptNV98OywqnfmcFgYtqep8wgHBpGTcROwxda0="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "darwin";
+        arch = "arm64";
+        sha256 = "sha256-egv4BKxRI6rfGnAiLjBlfmc5heyXUtP2LDbI5B4t/lo="; # aarch64-darwin
+      };
     };
-
-    handler = { pkgs, wrap, system, builders }:
-      wrap.genDownloadBuilders { dontUnpack = true; };
   };
 }
