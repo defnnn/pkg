5945c2975340bfcecdaf9500755add7ebac08424
amanibhavam <iam@defn.sh>
2023-02-16 06:41:32 +0000

    caddy latest

--- caddy/flake.nix
+++ caddy/flake.nix
@@ -1,55 +1,36 @@
 {
-  inputs = {
-    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
-  };
+  inputs.pkg.url = github:defn/pkg/0.0.141;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
+    url_template = input: "https://github.com/caddyserver/caddy/releases/download/v${input.vendor}/caddy_${input.vendor}_${input.os}_${input.arch}.tar.gz";
 
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
+    installPhase = { src }: ''
+      install -m 0755 -d $out $out/bin
+      install -m 0755 caddy $out/bin/caddy
+    '';
 
-    config = rec {
-      slug = builtins.readFile ./SLUG;
-      vendor = builtins.readFile ./VENDOR;
-      revision = builtins.readFile ./REVISION;
-      version = "${vendor}-${revision}";
-
-      url_template = input: "https://github.com/caddyserver/caddy/releases/download/v${input.version}/caddy_${input.version}_${input.os}_${input.arch}.tar.gz";
-
-      downloads = {
-        "x86_64-linux" = {
-          version = vendor;
-          os = "linux";
-          arch = "amd64";
-          sha256 = "sha256-4sO+CrSMR5AizEPjBiEe5GSaLTfjLAOU6rH0ZD/I6cY="; # x86_64-linux
-        };
-        "aarch64-linux" = {
-          version = vendor;
-          os = "linux";
-          arch = "arm64";
-          sha256 = "sha256-VSTFShq5MLp6bZZMO1pbotSr4RHDxSpbTcATAmOfo3c="; # aarch64-linux
-        };
-        "x86_64-darwin" = {
-          version = vendor;
-          os = "mac";
-          arch = "amd64";
-          sha256 = "sha256-ErvMpi7YB2W/KlMSsh2ckHKLcnmwHPsRve5snHi3Fts="; # x86_64-darwin
-        };
-        "aarch64-darwin" = {
-          version = vendor;
-          os = "mac";
-          arch = "arm64";
-          sha256 = "sha256-UdyQ9A1UYVheL0LUpetn+80p4k4YC9wvYwON56OGK7Y="; # aarch64-darwin
-        };
+    downloads = {
+      "x86_64-linux" = {
+        os = "linux";
+        arch = "amd64";
+        sha256 = "sha256-4sO+CrSMR5AizEPjBiEe5GSaLTfjLAOU6rH0ZD/I6cY="; # x86_64-linux
       };
-
-      installPhase = { src }: ''
-        install -m 0755 -d $out $out/bin
-        install -m 0755 caddy $out/bin/caddy
-      '';
+      "aarch64-linux" = {
+        os = "linux";
+        arch = "arm64";
+        sha256 = "sha256-VSTFShq5MLp6bZZMO1pbotSr4RHDxSpbTcATAmOfo3c="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "mac";
+        arch = "amd64";
+        sha256 = "sha256-ErvMpi7YB2W/KlMSsh2ckHKLcnmwHPsRve5snHi3Fts="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "mac";
+        arch = "arm64";
+        sha256 = "sha256-UdyQ9A1UYVheL0LUpetn+80p4k4YC9wvYwON56OGK7Y="; # aarch64-darwin
+      };
     };
-
-    handler = { pkgs, wrap, system, builders }:
-      wrap.genDownloadBuilders { };
   };
 }
