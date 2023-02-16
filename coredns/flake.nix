6f30f026df625c10b47c93b26e695be145dd3186
amanibhavam <iam@defn.sh>
2023-02-16 16:15:24 +0000

    coredns latest

--- coredns/flake.nix
+++ coredns/flake.nix
@@ -1,55 +1,38 @@
 {
-  inputs = {
-    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
-  };
+  inputs.pkg.url = github:defn/pkg/0.0.142;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
+    url_template = input: "https://github.com/coredns/coredns/releases/download/v${input.vendor}/coredns_${input.vendor}_${input.os}_${input.arch}.tgz";
 
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
+    installPhase = { src }: ''
+      install -m 0755 -d $out $out/bin
+      install -m 0755 coredns $out/bin/coredns
+    '';
 
-    config = rec {
-      slug = builtins.readFile ./SLUG;
-      vendor = builtins.readFile ./VENDOR;
-      revision = builtins.readFile ./REVISION;
-      version = "${vendor}-${revision}";
+    downloads = {
+      options = { };
 
-      url_template = input: "https://github.com/coredns/coredns/releases/download/v${input.version}/coredns_${input.version}_${input.os}_${input.arch}.tgz";
-
-      downloads = {
-        "x86_64-linux" = {
-          version = vendor;
-          os = "linux";
-          arch = "amd64";
-          sha256 = "sha256-mMtg4zWEObuz4/e+kW/hDFGx7ZR1j1OusrTAYVZhL/Y="; # x86_64-linux
-        };
-        "aarch64-linux" = {
-          version = vendor;
-          os = "linux";
-          arch = "arm64";
-          sha256 = "sha256-MasBAAfu9BlVHNee5x6wPR/STunVl9LuTKQHccTgtzw="; # aarch64-linux
-        };
-        "x86_64-darwin" = {
-          version = vendor;
-          os = "darwin";
-          arch = "amd64";
-          sha256 = "sha256-VZm7s39kVrzm69ojZhaZnGtytgySbKlxcrQIQe6Vu5k="; # x86_64-darwin
-        };
-        "aarch64-darwin" = {
-          version = vendor;
-          os = "darwin";
-          arch = "amd64";
-          sha256 = "sha256-VZm7s39kVrzm69ojZhaZnGtytgySbKlxcrQIQe6Vu5k="; # aarch64-darwin
-        };
+      "x86_64-linux" = {
+        os = "linux";
+        arch = "amd64";
+        sha256 = "sha256-mMtg4zWEObuz4/e+kW/hDFGx7ZR1j1OusrTAYVZhL/Y="; # x86_64-linux
       };
-
-      installPhase = { src }: ''
-        install -m 0755 -d $out $out/bin
-        install -m 0755 coredns $out/bin/coredns
-      '';
+      "aarch64-linux" = {
+        os = "linux";
+        arch = "arm64";
+        sha256 = "sha256-MasBAAfu9BlVHNee5x6wPR/STunVl9LuTKQHccTgtzw="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "darwin";
+        arch = "amd64";
+        sha256 = "sha256-VZm7s39kVrzm69ojZhaZnGtytgySbKlxcrQIQe6Vu5k="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "darwin";
+        arch = "amd64";
+        sha256 = "sha256-VZm7s39kVrzm69ojZhaZnGtytgySbKlxcrQIQe6Vu5k="; # aarch64-darwin
+      };
     };
-
-    handler = { pkgs, wrap, system, builders }:
-      wrap.genDownloadBuilders { };
   };
 }
