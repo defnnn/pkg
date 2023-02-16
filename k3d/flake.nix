769323e1a0e1f7b2cc042e1a05b0024f730ed3d0
amanibhavam <iam@defn.sh>
2023-02-16 16:13:19 +0000

    k3d latest

--- k3d/flake.nix
+++ k3d/flake.nix
@@ -1,55 +1,38 @@
 {
-  inputs = {
-    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
-  };
+  inputs.pkg.url = github:defn/pkg/0.0.142;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
+    url_template = input: "https://github.com/k3d-io/k3d/releases/download/v${input.vendor}/k3d-${input.os}-${input.arch}";
 
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
+    installPhase = { src }: ''
+      install -m 0755 -d $out $out/bin
+      install -m 0755 $src $out/bin/k3d
+    '';
 
-    config = rec {
-      slug = builtins.readFile ./SLUG;
-      revision = builtins.readFile ./REVISION;
-      vendor = builtins.readFile ./VENDOR;
-      version = "${vendor}-${revision}";
+    downloads = {
+      options = { dontUnpack = true; };
 
-      url_template = input: "https://github.com/k3d-io/k3d/releases/download/v${input.version}/k3d-${input.os}-${input.arch}";
-
-      downloads = {
-        "x86_64-linux" = {
-          version = vendor;
-          os = "linux";
-          arch = "amd64";
-          sha256 = "sha256-xUIY1b9studJh78h94IEg/WBsAxJ/uEdvbjBfHgpoCU="; # x86_64-linux
-        };
-        "aarch64-linux" = {
-          version = vendor;
-          os = "linux";
-          arch = "arm64";
-          sha256 = "sha256-yM61XtL/4gFbkVhI0Lbqpy28E1gnDe5ODBI1jBYjHUA="; # aarch64-linux
-        };
-        "x86_64-darwin" = {
-          version = vendor;
-          os = "darwin";
-          arch = "amd64";
-          sha256 = "sha256-imKlRXVRczX7rahFyvt1oFjJD3t76D532oZmAlB8qnE="; # x86_64-darwin
-        };
-        "aarch64-darwin" = {
-          version = vendor;
-          os = "darwin";
-          arch = "arm64";
-          sha256 = "sha256-yYEelLlpH1voRhIYOwDh8ij8tGM/Qd0hVXvLDKIFtFk="; # aarch64-darwin
-        };
+      "x86_64-linux" = {
+        os = "linux";
+        arch = "amd64";
+        sha256 = "sha256-xUIY1b9studJh78h94IEg/WBsAxJ/uEdvbjBfHgpoCU="; # x86_64-linux
       };
-
-      installPhase = { src }: ''
-        install -m 0755 -d $out $out/bin
-        install -m 0755 $src $out/bin/k3d
-      '';
+      "aarch64-linux" = {
+        os = "linux";
+        arch = "arm64";
+        sha256 = "sha256-yM61XtL/4gFbkVhI0Lbqpy28E1gnDe5ODBI1jBYjHUA="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "darwin";
+        arch = "amd64";
+        sha256 = "sha256-imKlRXVRczX7rahFyvt1oFjJD3t76D532oZmAlB8qnE="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "darwin";
+        arch = "arm64";
+        sha256 = "sha256-yYEelLlpH1voRhIYOwDh8ij8tGM/Qd0hVXvLDKIFtFk="; # aarch64-darwin
+      };
     };
-
-    handler = { pkgs, wrap, system, builders }:
-      wrap.genDownloadBuilders { dontUnpack = true; };
   };
 }
