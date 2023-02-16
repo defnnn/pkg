e840acfcb39b3a866edac03548a71e3d82060cb0
amanibhavam <iam@defn.sh>
2023-02-16 05:21:16 +0000

    helm: latest

--- helm/flake.nix
+++ helm/flake.nix
@@ -1,36 +1,35 @@
 {
-  inputs.pkg.url = github:defn/pkg/0.0.133;
+  inputs.pkg.url = github:defn/pkg/0.0.139;
   outputs = inputs: inputs.pkg.downloadMain {
     src = ./.;
-    config = rec {
-      url_template = input: "https://get.helm.sh/helm-v${input.vendor}-${input.os}-${input.arch}.tar.gz";
 
-      installPhase = { src }: ''
-        install -m 0755 -d $out $out/bin
-        install -m 0755 */helm $out/bin/helm
-      '';
+    url_template = input: "https://get.helm.sh/helm-v${input.vendor}-${input.os}-${input.arch}.tar.gz";
 
-      downloads = {
-        "x86_64-linux" = {
-          os = "linux";
-          arch = "amd64";
-          sha256 = "sha256-Cxvpa2b6tHcFJvE29fGjhaR8QZI9M6qw3LUA4PbBv3w="; # x86_64-linux
-        };
-        "aarch64-linux" = {
-          os = "linux";
-          arch = "arm64";
-          sha256 = "sha256-kZFz6Pt6O1TXavn+uS5J6G1agMUYUCC66MOT+g8N4eg="; # aarch64-linux
-        };
-        "x86_64-darwin" = {
-          os = "darwin";
-          arch = "amd64";
-          sha256 = "sha256-JUipDlzJV8zFAWtHBgZlqdLNTVtNYdzDL13jFE0QOCY="; # x86_64-darwin
-        };
-        "aarch64-darwin" = {
-          os = "darwin";
-          arch = "arm64";
-          sha256 = "sha256-Q9AZinouomOcqvqBuwWWyXvuLU5A31CzYgI0PrTVxGs="; # aarch64-darwin
-        };
+    installPhase = { src }: ''
+      install -m 0755 -d $out $out/bin
+      install -m 0755 */helm $out/bin/helm
+    '';
+
+    downloads = {
+      "x86_64-linux" = {
+        os = "linux";
+        arch = "amd64";
+        sha256 = "sha256-Cxvpa2b6tHcFJvE29fGjhaR8QZI9M6qw3LUA4PbBv3w="; # x86_64-linux
+      };
+      "aarch64-linux" = {
+        os = "linux";
+        arch = "arm64";
+        sha256 = "sha256-kZFz6Pt6O1TXavn+uS5J6G1agMUYUCC66MOT+g8N4eg="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "darwin";
+        arch = "amd64";
+        sha256 = "sha256-JUipDlzJV8zFAWtHBgZlqdLNTVtNYdzDL13jFE0QOCY="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "darwin";
+        arch = "arm64";
+        sha256 = "sha256-Q9AZinouomOcqvqBuwWWyXvuLU5A31CzYgI0PrTVxGs="; # aarch64-darwin
       };
     };
   };
