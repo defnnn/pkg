34501e40906e238320c742759385ff6f1769b238
amanibhavam <iam@defn.sh>
2023-02-16 16:01:18 +0000

    earthly latest

--- earthly/flake.nix
+++ earthly/flake.nix
@@ -1,55 +1,40 @@
 {
-  inputs = {
-    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
-  };
+  inputs.pkg.url = github:defn/pkg/0.0.142;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
+    url_template = input: "https://github.com/earthly/earthly/releases/download/v${input.vendor}/earthly-${input.os}-${input.arch}";
 
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
+    installPhase = { src }: ''
+      install -m 0755 -d $out $out/bin
+      install -m 0755 $src $out/bin/earthly
+    '';
 
-    config = rec {
-      slug = builtins.readFile ./SLUG;
-      revision = builtins.readFile ./REVISION;
-      vendor = builtins.readFile ./VENDOR;
-      version = "${vendor}-${revision}";
-
-      url_template = input: "https://github.com/earthly/earthly/releases/download/v${input.version}/earthly-${input.os}-${input.arch}";
-
-      downloads = {
-        "x86_64-linux" = {
-          version = vendor;
-          os = "linux";
-          arch = "amd64";
-          sha256 = "sha256-B/uohgfBTj/X9fJ2i6XmkwP2zGN6k2PVoOyVB7maRq4="; # x86_64-linux
-        };
-        "aarch64-linux" = {
-          version = vendor;
-          os = "linux";
-          arch = "arm64";
-          sha256 = "sha256-n2CygpKsZ1HPFDh20x6bUeWYs7U7hMlQRsfgX409JFk="; # aarch64-linux
-        };
-        "x86_64-darwin" = {
-          version = vendor;
-          os = "darwin";
-          arch = "amd64";
-          sha256 = "sha256-5Fw0+mnJBk5Y7+9Z1Z1LVORtNvqOn46b6A34OjGfV+Y="; # x86_64-darwin
-        };
-        "aarch64-darwin" = {
-          version = vendor;
-          os = "darwin";
-          arch = "arm64";
-          sha256 = "sha256-6VJlPBIZmXpCl+rW2fS/ZlmmF0qyYjCzct+LVh8vjEk="; # aarch64-darwin
-        };
+    downloads = {
+      options = {
+        dontUnpack = true;
       };
 
-      installPhase = { src }: ''
-        install -m 0755 -d $out $out/bin
-        install -m 0755 $src $out/bin/earthly
-      '';
+      "x86_64-linux" = {
+        os = "linux";
+        arch = "amd64";
+        sha256 = "sha256-B/uohgfBTj/X9fJ2i6XmkwP2zGN6k2PVoOyVB7maRq4="; # x86_64-linux
+      };
+      "aarch64-linux" = {
+        os = "linux";
+        arch = "arm64";
+        sha256 = "sha256-n2CygpKsZ1HPFDh20x6bUeWYs7U7hMlQRsfgX409JFk="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "darwin";
+        arch = "amd64";
+        sha256 = "sha256-5Fw0+mnJBk5Y7+9Z1Z1LVORtNvqOn46b6A34OjGfV+Y="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "darwin";
+        arch = "arm64";
+        sha256 = "sha256-6VJlPBIZmXpCl+rW2fS/ZlmmF0qyYjCzct+LVh8vjEk="; # aarch64-darwin
+      };
     };
-
-    handler = { pkgs, wrap, system, builders }:
-      wrap.genDownloadBuilders { dontUnpack = true; };
   };
 }
