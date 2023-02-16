244cd38e1995929e69eb6cc0c174c9beb5879c5e
amanibhavam <iam@defn.sh>
2023-02-16 06:37:49 +0000

    step: latest

--- step/flake.nix
+++ step/flake.nix
@@ -1,55 +1,38 @@
 {
-  inputs = {
-    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
-  };
+  inputs.pkg.url = github:defn/pkg/0.0.141;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
+    url_template = input: "https://github.com/smallstep/cli/releases/download/v${input.vendor}/step_${input.os}_${input.vendor}_${input.arch}.tar.gz";
 
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
+    installPhase = { src }: ''
+      install -m 0755 -d $out $out/bin
+      install -m 0755 */bin/step $out/bin/step
+    '';
 
-    config = rec {
-      slug = builtins.readFile ./SLUG;
-      vendor = builtins.readFile ./VENDOR;
-      revision = builtins.readFile ./REVISION;
-      version = "${vendor}-${revision}";
+    downloads = {
+      options = { };
 
-      url_template = input: "https://github.com/smallstep/cli/releases/download/v${input.version}/step_${input.os}_${input.version}_${input.arch}.tar.gz";
-
-      downloads = {
-        "x86_64-linux" = {
-          version = vendor;
-          os = "linux";
-          arch = "amd64";
-          sha256 = "sha256-BE/lUX7OkH3ZGT6SuhV5kmMQ4GlOg/6Ue0KOJP8Il4U="; # x86_64-linux
-        };
-        "aarch64-linux" = {
-          version = vendor;
-          os = "linux";
-          arch = "arm64";
-          sha256 = "sha256-FrRDsnR69U9BtIKpiiRczG1A9fbRBeNsMZApIAmoNyA="; # aarch64-linux
-        };
-        "x86_64-darwin" = {
-          version = vendor;
-          os = "darwin";
-          arch = "amd64";
-          sha256 = "sha256-p4PZAd5NBOporuNOcBZZqwjEGCn1hFb0PfNXIjbKqW4="; # x86_64-darwin
-        };
-        "aarch64-darwin" = {
-          version = vendor;
-          os = "darwin";
-          arch = "arm64";
-          sha256 = "sha256-aIcI8zPPkFV3PvRgxTUWZnsHsJIVHqWMvPa55nWqX2o="; # aarch64-darwin
-        };
+      "x86_64-linux" = {
+        os = "linux";
+        arch = "amd64";
+        sha256 = "sha256-BE/lUX7OkH3ZGT6SuhV5kmMQ4GlOg/6Ue0KOJP8Il4U="; # x86_64-linux
       };
-
-      installPhase = { src }: ''
-        install -m 0755 -d $out $out/bin
-        install -m 0755 */bin/step $out/bin/step
-      '';
+      "aarch64-linux" = {
+        os = "linux";
+        arch = "arm64";
+        sha256 = "sha256-FrRDsnR69U9BtIKpiiRczG1A9fbRBeNsMZApIAmoNyA="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "darwin";
+        arch = "amd64";
+        sha256 = "sha256-p4PZAd5NBOporuNOcBZZqwjEGCn1hFb0PfNXIjbKqW4="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "darwin";
+        arch = "arm64";
+        sha256 = "sha256-aIcI8zPPkFV3PvRgxTUWZnsHsJIVHqWMvPa55nWqX2o="; # aarch64-darwin
+      };
     };
-
-    handler = { pkgs, wrap, system, builders }:
-      wrap.genDownloadBuilders { };
   };
 }
