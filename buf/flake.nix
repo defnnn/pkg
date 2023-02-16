755b379950c4f438102ef8fe4842f93478367b61
amanibhavam <iam@defn.sh>
2023-02-16 06:39:37 +0000

    buf latest

--- buf/flake.nix
+++ buf/flake.nix
@@ -1,55 +1,36 @@
 {
-  inputs = {
-    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
-  };
+  inputs.pkg.url = github:defn/pkg/0.0.141;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
+    url_template = input: "https://github.com/bufbuild/buf/releases/download/v${input.vendor}/buf-${input.os}-${input.arch}.tar.gz";
 
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
+    installPhase = { src }: ''
+      install -m 0755 -d $out $out/bin
+      install -m 0755 buf/bin/buf $out/bin/buf
+    '';
 
-    config = rec {
-      slug = builtins.readFile ./SLUG;
-      vendor = builtins.readFile ./VENDOR;
-      revision = builtins.readFile ./REVISION;
-      version = "${vendor}-${revision}";
-
-      url_template = input: "https://github.com/bufbuild/buf/releases/download/v${input.version}/buf-${input.os}-${input.arch}.tar.gz";
-
-      downloads = {
-        "x86_64-linux" = {
-          version = vendor;
-          os = "Linux";
-          arch = "x86_64";
-          sha256 = "sha256-mrOCCBhy3wP6rxks+oJWbTJDbP14eCA16UtNBKmCYg8="; # x86_64-linux
-        };
-        "aarch64-linux" = {
-          version = vendor;
-          os = "Linux";
-          arch = "aarch64";
-          sha256 = "sha256-HdAI5oEAYlnXzjJ1+0nH30XS+eoPszRpgKh2PqHwZZk="; # aarch64-linux
-        };
-        "x86_64-darwin" = {
-          version = vendor;
-          os = "Darwin";
-          arch = "x86_64";
-          sha256 = "sha256-wwLMpd2XHPftOpYAvdChUY0Pb2MWCPlbKK4xCdAQ9Gc="; # x86_64-darwin
-        };
-        "aarch64-darwin" = {
-          version = vendor;
-          os = "Darwin";
-          arch = "x86_64";
-          sha256 = "sha256-wwLMpd2XHPftOpYAvdChUY0Pb2MWCPlbKK4xCdAQ9Gc="; # aarch64-darwin
-        };
+    downloads = {
+      "x86_64-linux" = {
+        os = "Linux";
+        arch = "x86_64";
+        sha256 = "sha256-mrOCCBhy3wP6rxks+oJWbTJDbP14eCA16UtNBKmCYg8="; # x86_64-linux
       };
-
-      installPhase = { src }: ''
-        install -m 0755 -d $out $out/bin
-        install -m 0755 buf/bin/buf $out/bin/buf
-      '';
+      "aarch64-linux" = {
+        os = "Linux";
+        arch = "aarch64";
+        sha256 = "sha256-HdAI5oEAYlnXzjJ1+0nH30XS+eoPszRpgKh2PqHwZZk="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "Darwin";
+        arch = "x86_64";
+        sha256 = "sha256-wwLMpd2XHPftOpYAvdChUY0Pb2MWCPlbKK4xCdAQ9Gc="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "Darwin";
+        arch = "x86_64";
+        sha256 = "sha256-wwLMpd2XHPftOpYAvdChUY0Pb2MWCPlbKK4xCdAQ9Gc="; # aarch64-darwin
+      };
     };
-
-    handler = { pkgs, wrap, system, builders }:
-      wrap.genDownloadBuilders { };
   };
 }
