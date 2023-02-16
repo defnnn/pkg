d7c4b170a853294071473ac73892fc6ec56fb34b
amanibhavam <iam@defn.sh>
2023-02-16 06:51:00 +0000

    flytctl latest

--- flyctl/flake.nix
+++ flyctl/flake.nix
@@ -1,55 +1,38 @@
 {
-  inputs = {
-    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
-  };
+  inputs.pkg.url = github:defn/pkg/0.0.141;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
+    url_template = input: "https://github.com/superfly/flyctl/releases/download/v${input.vendor}/flyctl_${input.vendor}_${input.os}_${input.arch}.tar.gz";
 
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
+    installPhase = { src }: ''
+      install -m 0755 -d $out $out/bin
+      install -m 0755 flyctl $out/bin/flyctl
+    '';
 
-    config = rec {
-      slug = builtins.readFile ./SLUG;
-      vendor = builtins.readFile ./VENDOR;
-      revision = builtins.readFile ./REVISION;
-      version = "${vendor}-${revision}";
+    downloads = {
+      options = { };
 
-      url_template = input: "https://github.com/superfly/flyctl/releases/download/v${input.version}/flyctl_${input.version}_${input.os}_${input.arch}.tar.gz";
-
-      downloads = {
-        "x86_64-linux" = {
-          version = vendor;
-          os = "Linux";
-          arch = "x86_64";
-          sha256 = "sha256-ZcWIW+zd+pHeju9264tQa3a66vO85ulvMT2buo005kk="; # x86_64-linux
-        };
-        "aarch64-linux" = {
-          version = vendor;
-          os = "Linux";
-          arch = "arm64";
-          sha256 = "sha256-8B24OMcV+MoEUL93qXrr+vF1fDgsa0xyvVx6gO+YzBg="; # aarch64-linux
-        };
-        "x86_64-darwin" = {
-          version = vendor;
-          os = "macOS";
-          arch = "x86_64";
-          sha256 = "sha256-dEJdTgh2A5av15CEvE8z4sbwXJdFxI2MakEY01w/r/0="; # x86_64-darwin
-        };
-        "aarch64-darwin" = {
-          version = vendor;
-          os = "macOS";
-          arch = "arm64";
-          sha256 = "sha256-9Md5993zxKnHsYEf2I1RpJocHen9Qxwr0IgcQXOunZ4="; # aarch64-darwin
-        };
+      "x86_64-linux" = {
+        os = "Linux";
+        arch = "x86_64";
+        sha256 = "sha256-ZcWIW+zd+pHeju9264tQa3a66vO85ulvMT2buo005kk="; # x86_64-linux
       };
-
-      installPhase = { src }: ''
-        install -m 0755 -d $out $out/bin
-        install -m 0755 flyctl $out/bin/flyctl
-      '';
+      "aarch64-linux" = {
+        os = "Linux";
+        arch = "arm64";
+        sha256 = "sha256-8B24OMcV+MoEUL93qXrr+vF1fDgsa0xyvVx6gO+YzBg="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "macOS";
+        arch = "x86_64";
+        sha256 = "sha256-dEJdTgh2A5av15CEvE8z4sbwXJdFxI2MakEY01w/r/0="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "macOS";
+        arch = "arm64";
+        sha256 = "sha256-9Md5993zxKnHsYEf2I1RpJocHen9Qxwr0IgcQXOunZ4="; # aarch64-darwin
+      };
     };
-
-    handler = { pkgs, wrap, system, builders }:
-      wrap.genDownloadBuilders { };
   };
 }
