577b5dc82c461351faac269a2ae39f188f5560ca
amanibhavam <iam@defn.sh>
2023-02-16 16:11:12 +0000

    glow latest

--- glow/flake.nix
+++ glow/flake.nix
@@ -1,55 +1,38 @@
 {
-  inputs = {
-    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
-  };
+  inputs.pkg.url = github:defn/pkg/0.0.142;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
+    url_template = input: "https://github.com/charmbracelet/glow/releases/download/v${input.vendor}/glow_${input.vendor}_${input.os}_${input.arch}.tar.gz";
 
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
+    installPhase = { src }: ''
+      install -m 0755 -d $out $out/bin
+      install -m 0755 glow $out/bin/glow
+    '';
 
-    config = rec {
-      slug = builtins.readFile ./SLUG;
-      vendor = builtins.readFile ./VENDOR;
-      revision = builtins.readFile ./REVISION;
-      version = "${vendor}-${revision}";
+    downloads = {
+      options = { };
 
-      url_template = input: "https://github.com/charmbracelet/glow/releases/download/v${input.version}/glow_${input.version}_${input.os}_${input.arch}.tar.gz";
-
-      downloads = {
-        "x86_64-linux" = {
-          version = vendor;
-          os = "Linux";
-          arch = "x86_64";
-          sha256 = "sha256-ROAqfgK23yOYMR6lpgc9NcJkmxCbacF0A5Gyv/J3CR0="; # x86_64-linux
-        };
-        "aarch64-linux" = {
-          version = vendor;
-          os = "Linux";
-          arch = "arm64";
-          sha256 = "sha256-BYH96qkB8Gwc2xqoZgWlLCdizNTQsFrvzirB9EyC9MU="; # aarch64-linux
-        };
-        "x86_64-darwin" = {
-          version = vendor;
-          os = "Darwin";
-          arch = "x86_64";
-          sha256 = "sha256-oE1fR1mtEymWUvA2L+hYbKd7OZ5m+Jm9DJ9zqNTcRmE="; # x86_64-darwin
-        };
-        "aarch64-darwin" = {
-          version = vendor;
-          os = "Darwin";
-          arch = "arm64";
-          sha256 = "sha256-6swuY8BjgMoOXJmjzqjJ6rUhLTeCMCHmd9HyqPfURkE="; # aarch64-darwin
-        };
+      "x86_64-linux" = {
+        os = "Linux";
+        arch = "x86_64";
+        sha256 = "sha256-ROAqfgK23yOYMR6lpgc9NcJkmxCbacF0A5Gyv/J3CR0="; # x86_64-linux
       };
-
-      installPhase = { src }: ''
-        install -m 0755 -d $out $out/bin
-        install -m 0755 glow $out/bin/glow
-      '';
+      "aarch64-linux" = {
+        os = "Linux";
+        arch = "arm64";
+        sha256 = "sha256-BYH96qkB8Gwc2xqoZgWlLCdizNTQsFrvzirB9EyC9MU="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "Darwin";
+        arch = "x86_64";
+        sha256 = "sha256-oE1fR1mtEymWUvA2L+hYbKd7OZ5m+Jm9DJ9zqNTcRmE="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "Darwin";
+        arch = "arm64";
+        sha256 = "sha256-6swuY8BjgMoOXJmjzqjJ6rUhLTeCMCHmd9HyqPfURkE="; # aarch64-darwin
+      };
     };
-
-    handler = { pkgs, wrap, system, builders }:
-      wrap.genDownloadBuilders { };
   };
 }
