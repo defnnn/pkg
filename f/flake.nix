12b78a64f342e030d5c2e8f5efe59d16f0a7d37e
amanibhavam <iam@defn.sh>
2023-02-16 02:23:37 +0000

    use pkg/main

--- f/flake.nix
+++ f/flake.nix
@@ -1,22 +1,15 @@
 {
-  inputs = {
-    dev.url = github:defn/pkg/dev-0.0.23?dir=dev;
-  };
+  inputs.pkg.url = github:defn/pkg/0.0.121;
+  outputs = inputs: inputs.pkg.main rec {
+    src = ./.;
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
-
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
-
-    handler = { pkgs, wrap, system, builders, commands, config }: {
-      defaultPackage = wrap.bashBuilder {
-        src = ./bin;
-        installPhase = ''
-          mkdir -p $out/bin
-          cp -a $src/* $out/bin/
-          chmod 755 $out/bin/f $out/bin/f-*
-        '';
-      };
+    defaultPackage = ctx: ctx.wrap.bashBuilder {
+      inherit src;
+      installPhase = ''
+        mkdir -p $out/bin
+        cp -a $src/bin/* $out/bin/
+        chmod 755 $out/bin/tf $out/bin/f-*
+      '';
     };
   };
 }
