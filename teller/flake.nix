134fc40c88e1ac2ec1848a97768fadc45f4a7ee6
amanibhavam <iam@defn.sh>
2023-02-16 15:59:16 +0000

    teller latest

--- teller/flake.nix
+++ teller/flake.nix
@@ -1,55 +1,37 @@
 {
-  inputs = {
-    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
-  };
+  inputs.pkg.url = github:defn/pkg/0.0.142;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
+    url_template = input: "https://github.com/tellerops/teller/releases/download/v${input.vendor}/teller_${input.vendor}_${input.os}_${input.arch}.tar.gz";
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
+    installPhase = { src }: ''
+      install -m 0755 -d $out $out/bin
+      install -m 0755 teller $out/bin/teller
+    '';
 
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
+    downloads = {
+      options = { };
 
-    config = rec {
-      slug = builtins.readFile ./SLUG;
-      vendor = builtins.readFile ./VENDOR;
-      revision = builtins.readFile ./REVISION;
-      version = "${vendor}-${revision}";
-
-      url_template = input: "https://github.com/tellerops/teller/releases/download/v${input.version}/teller_${input.version}_${input.os}_${input.arch}.tar.gz";
-
-      downloads = {
-        "x86_64-linux" = {
-          version = vendor;
-          os = "Linux";
-          arch = "x86_64";
-          sha256 = "sha256-VyT1DsFfCRx+hVeyEvUhbEk77jDkevVK5UMK4y3e7hQ="; # x86_64-linux
-        };
-        "aarch64-linux" = {
-          version = vendor;
-          os = "Linux";
-          arch = "arm64";
-          sha256 = "sha256-X00ApBJt9uh13tuBZp7HHhACufewiP0XWCYfA6Z9pnk="; # aarch64-linux
-        };
-        "x86_64-darwin" = {
-          version = vendor;
-          os = "Darwin";
-          arch = "x86_64";
-          sha256 = "sha256-AEia8MpO3Ypi4dRl48AvtNr8ZJXzq/LZbGovTFYbXdQ="; # x86_64-darwin
-        };
-        "aarch64-darwin" = {
-          version = vendor;
-          os = "Darwin";
-          arch = "arm64";
-          sha256 = "sha256-/d7sduvkUXvL26J6rr7yrTxpzX0ciGj5Iiv3363WJ+c="; # aarch64-darwin
-        };
+      "x86_64-linux" = {
+        os = "Linux";
+        arch = "x86_64";
+        sha256 = "sha256-VyT1DsFfCRx+hVeyEvUhbEk77jDkevVK5UMK4y3e7hQ="; # x86_64-linux
       };
-
-      installPhase = { src }: ''
-        install -m 0755 -d $out $out/bin
-        install -m 0755 teller $out/bin/teller
-      '';
+      "aarch64-linux" = {
+        os = "Linux";
+        arch = "arm64";
+        sha256 = "sha256-X00ApBJt9uh13tuBZp7HHhACufewiP0XWCYfA6Z9pnk="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "Darwin";
+        arch = "x86_64";
+        sha256 = "sha256-AEia8MpO3Ypi4dRl48AvtNr8ZJXzq/LZbGovTFYbXdQ="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "Darwin";
+        arch = "arm64";
+        sha256 = "sha256-/d7sduvkUXvL26J6rr7yrTxpzX0ciGj5Iiv3363WJ+c="; # aarch64-darwin
+      };
     };
-
-    handler = { pkgs, wrap, system, builders }:
-      wrap.genDownloadBuilders { };
   };
 }
