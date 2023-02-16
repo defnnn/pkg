9d9822bfe429e23602317c3d7c0ca46210209d3d
amanibhavam <iam@defn.sh>
2023-02-16 06:30:58 +0000

    tilt: latest

--- tilt/flake.nix
+++ tilt/flake.nix
@@ -1,56 +1,38 @@
 {
-  inputs = {
-    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
-  };
+  inputs.pkg.url = github:defn/pkg/0.0.141;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
+    url_template = input: "https://github.com/tilt-dev/tilt/releases/download/v${input.vendor}/tilt.${input.vendor}.${input.os}.${input.arch}.tar.gz";
 
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
+    installPhase = { src }: ''
+      install -m 0755 -d $out $out/bin
+      install -m 0755 tilt $out/bin/tilt
+    '';
 
-    config = rec {
-      slug = builtins.readFile ./SLUG;
-      vendor = builtins.readFile ./VENDOR;
-      revision = builtins.readFile ./REVISION;
-      version = "${vendor}-${revision}";
+    downloads = {
+      options = { };
 
-      url_template = input: "https://github.com/tilt-dev/tilt/releases/download/v${input.version}/tilt.${input.version}.${input.os}.${input.arch}.tar.gz";
-
-      downloads = {
-        "x86_64-linux" = {
-          version = vendor;
-          os = "linux";
-          arch = "x86_64";
-          sha256 = "sha256-nsGiGTkzZ/xB3V9fEIP2DW6YOeWCG4FwwmSBv2sZ3H4="; # x86_64-linux
-        };
-        "aarch64-linux" = {
-          version = vendor;
-          os = "linux";
-          arch = "arm64";
-          sha256 = "sha256-ig/KOynkgr08WPwXd+S+BfUIcjpkgklIeKJ199Wn8sE="; # aarch64-linux
-        };
-        "x86_64-darwin" = {
-          version = vendor;
-          os = "mac";
-          arch = "x86_64";
-          sha256 = "sha256-reCHfnL1agiTd+UX78hjaSIKx7aoHweOjpmpwpqiROs="; # x86_64-darwin
-        };
-        "aarch64-darwin" = {
-          version = vendor;
-          os = "mac";
-          arch = "arm64";
-          sha256 = "sha256-2h1bSzspBY1682izyTWksdsrHyXavNlfwcQzA65zZf0="; # aarch64-darwin
-        };
+      "x86_64-linux" = {
+        os = "linux";
+        arch = "x86_64";
+        sha256 = "sha256-nsGiGTkzZ/xB3V9fEIP2DW6YOeWCG4FwwmSBv2sZ3H4="; # x86_64-linux
       };
-
-      installPhase = { src }: ''
-        install -m 0755 -d $out $out/bin
-        install -m 0755 tilt $out/bin/tilt
-      '';
-
+      "aarch64-linux" = {
+        os = "linux";
+        arch = "arm64";
+        sha256 = "sha256-ig/KOynkgr08WPwXd+S+BfUIcjpkgklIeKJ199Wn8sE="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "mac";
+        arch = "x86_64";
+        sha256 = "sha256-reCHfnL1agiTd+UX78hjaSIKx7aoHweOjpmpwpqiROs="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "mac";
+        arch = "arm64";
+        sha256 = "sha256-2h1bSzspBY1682izyTWksdsrHyXavNlfwcQzA65zZf0="; # aarch64-darwin
+      };
     };
-
-    handler = { pkgs, wrap, system, builders }:
-      wrap.genDownloadBuilders { };
   };
 }
