dadbaa20e0dc373e7a1c1f08e933dd17829c29a2
amanibhavam <iam@defn.sh>
2023-02-18 19:20:21 +0000

    codeserver latest

--- codeserver/flake.nix
+++ codeserver/flake.nix
@@ -1,71 +1,52 @@
 {
-  inputs = {
-    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
-  };
+  inputs.pkg.url = github:defn/pkg/0.0.151;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
-
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
-
-    config = rec {
-      slug = builtins.readFile ./SLUG;
-      revision = builtins.readFile ./REVISION;
-      vendor = builtins.readFile ./VENDOR;
-      version = "${vendor}-${revision}";
-
-      url_template = input: "https://github.com/coder/code-server/releases/download/v${input.version}/code-server-${input.version}-${input.os}-${input.arch}.tar.gz";
-
-      downloads = {
-        "x86_64-linux" = {
-          version = vendor;
-          os = "linux";
-          arch = "amd64";
-          sha256 = "sha256-OymtjnW0iYCTPOzqliGMB9Oox5SSuzHZZlsE+aHorqQ="; # x86_64-linux
-        };
-        "aarch64-linux" = {
-          version = vendor;
-          os = "linux";
-          arch = "arm64";
-          sha256 = "sha256-ge5xFehQo7gf9I4E1aMe6inGhgBB2ioJjzGCkYC7as0="; # aarch64-linux
-        };
-        "x86_64-darwin" = {
-          version = vendor;
-          os = "macos";
-          arch = "amd64";
-          sha256 = "sha256-wkrx+mFwe6WjlU73bfMFXcC4eFQ2kapilgSC0Ds1fOE="; # x86_64-darwin
-        };
-        "aarch64-darwin" = {
-          version = vendor;
-          os = "macos";
-          arch = "amd64"; # code-server not avaialble for darwin arm64
-          sha256 = "sha256-wkrx+mFwe6WjlU73bfMFXcC4eFQ2kapilgSC0Ds1fOE="; # aarch64-darwin
-        };
+    extend = { ctx, this }: {
+      apps.default = {
+        type = "app";
+        program = "${this.defaultPackage}/bin/code-server";
       };
-
-      installPhase = { src }: ''
-        set -x
-        install -m 0755 -d $out $out/bin $out/lib
-        rsync -ia . $out/lib/.
-        mv -f $out/lib/code-server-${vendor}-*  $out/lib/code-server-${vendor}
-        ln -fs $out/lib/code-server-${vendor}/bin/code-server $out/bin/code-server
-        ln -fs $out/lib/code-server-${vendor}/lib/vscode/bin/helpers/browser.sh $out/bin/browser.sh
-      '';
     };
 
-    handler = { pkgs, wrap, system, builders }:
-      let
-        this = wrap.genDownloadBuilders
-          {
-            dontFixup = true;
-            buildInputs = with pkgs; [ rsync ];
-          };
-      in
-      this // {
-        apps.default = {
-          type = "app";
-          program = "${this.defaultPackage}/bin/code-server";
-        };
+    url_template = input: "https://github.com/coder/code-server/releases/download/v${input.vendor}/code-server-${input.vendor}-${input.os}-${input.arch}.tar.gz";
+
+    installPhase = { src, config }:
+      ''
+        install -m 0755 -d $out $out/bin $out/lib
+        rsync -ia . $out/lib/.
+        mv -f $out/lib/code-server-${config.vendor}-*  $out/lib/code-server-${config.vendor}
+        ln -fs $out/lib/code-server-${config.vendor}/bin/code-server $out/bin/code-server
+        ln -fs $out/lib/code-server-${config.vendor}/lib/vscode/bin/helpers/browser.sh $out/bin/browser.sh
+      '';
+
+    downloads = {
+      options = { ctx }: {
+        dontFixup = true;
+        buildInputs = with ctx.pkgs; [ rsync ];
       };
+
+      "x86_64-linux" = {
+        os = "linux";
+        arch = "amd64";
+        sha256 = "sha256-OymtjnW0iYCTPOzqliGMB9Oox5SSuzHZZlsE+aHorqQ="; # x86_64-linux
+      };
+      "aarch64-linux" = {
+        os = "linux";
+        arch = "arm64";
+        sha256 = "sha256-ge5xFehQo7gf9I4E1aMe6inGhgBB2ioJjzGCkYC7as0="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "macos";
+        arch = "amd64";
+        sha256 = "sha256-wkrx+mFwe6WjlU73bfMFXcC4eFQ2kapilgSC0Ds1fOE="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "macos";
+        arch = "amd64"; # code-server not avaialble for darwin arm64
+        sha256 = "sha256-wkrx+mFwe6WjlU73bfMFXcC4eFQ2kapilgSC0Ds1fOE="; # aarch64-darwin
+      };
+    };
   };
 }
