f803509320af8fb4cde021168930b62e3d69f1bd
amanibhavam <iam@defn.sh>
2023-02-18 04:14:27 +0000

    goreleaser latest

--- goreleaser/flake.nix
+++ goreleaser/flake.nix
@@ -1,55 +1,38 @@
 {
-  inputs = {
-    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
-  };
+  inputs.pkg.url = github:defn/pkg/0.0.147;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
+    url_template = input: "https://github.com/goreleaser/goreleaser/releases/download/v${input.vendor}/goreleaser_${input.os}_${input.arch}.tar.gz";
 
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
+    installPhase = { src }: ''
+      install -m 0755 -d $out $out/bin
+      install -m 0755 goreleaser $out/bin/goreleaser
+    '';
 
-    config = rec {
-      slug = builtins.readFile ./SLUG;
-      vendor = builtins.readFile ./VENDOR;
-      revision = builtins.readFile ./REVISION;
-      version = "${vendor}-${revision}";
+    downloads = {
+      options = ctx: { };
 
-      url_template = input: "https://github.com/goreleaser/goreleaser/releases/download/v${input.version}/goreleaser_${input.os}_${input.arch}.tar.gz";
-
-      downloads = {
-        "x86_64-linux" = {
-          version = vendor;
-          os = "linux";
-          arch = "x86_64";
-          sha256 = "sha256-FzDERVdW2Aclw2dS52oOIAqyaopits2qv/ACVzw4Kqk="; # x86_64-linux
-        };
-        "aarch64-linux" = {
-          version = vendor;
-          os = "linux";
-          arch = "arm64";
-          sha256 = "sha256-Qb8K+i617kUuHt7CqaVGZm42GhRtoyNpX24PclC7kWg="; # aarch64-linux
-        };
-        "x86_64-darwin" = {
-          version = vendor;
-          os = "darwin";
-          arch = "x86_64";
-          sha256 = "sha256-VPSxJfpXuRPzj/PcPPW1wwhft1a6tIVly7iaUgZdM10="; # x86_64-darwin
-        };
-        "aarch64-darwin" = {
-          version = vendor;
-          os = "darwin";
-          arch = "arm64";
-          sha256 = "sha256-h6On91aQF1jA04KatKhJHmslYoYViI3AOr8gi541/cM="; # aarch64-darwin
-        };
+      "x86_64-linux" = {
+        os = "linux";
+        arch = "x86_64";
+        sha256 = "sha256-FzDERVdW2Aclw2dS52oOIAqyaopits2qv/ACVzw4Kqk="; # x86_64-linux
       };
-
-      installPhase = { src }: ''
-        install -m 0755 -d $out $out/bin
-        install -m 0755 goreleaser $out/bin/goreleaser
-      '';
+      "aarch64-linux" = {
+        os = "linux";
+        arch = "arm64";
+        sha256 = "sha256-Qb8K+i617kUuHt7CqaVGZm42GhRtoyNpX24PclC7kWg="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "darwin";
+        arch = "x86_64";
+        sha256 = "sha256-VPSxJfpXuRPzj/PcPPW1wwhft1a6tIVly7iaUgZdM10="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "darwin";
+        arch = "arm64";
+        sha256 = "sha256-h6On91aQF1jA04KatKhJHmslYoYViI3AOr8gi541/cM="; # aarch64-darwin
+      };
     };
-
-    handler = { pkgs, wrap, system, builders }:
-      wrap.genDownloadBuilders { };
   };
 }
