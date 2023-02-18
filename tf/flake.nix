0084b3a96100ef8b749bd42d1634d98be7506bb4
amanibhavam <iam@defn.sh>
2023-02-18 20:06:07 +0000

    tf latest

--- tf/flake.nix
+++ tf/flake.nix
@@ -1,10 +1,13 @@
 {
-  inputs.pkg.url = github:defn/pkg/0.0.121;
+  inputs.pkg.url = github:defn/pkg/0.0.153;
   outputs = inputs: inputs.pkg.main rec {
     src = ./.;
 
+    extend = pkg: { };
+
     defaultPackage = ctx: ctx.wrap.bashBuilder {
       inherit src;
+
       installPhase = ''
         mkdir -p $out/bin
         cp -a $src/bin/* $out/bin/
