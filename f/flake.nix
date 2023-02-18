cee7cb097bc9139962a8ed143e232de8c5d5b509
amanibhavam <iam@defn.sh>
2023-02-18 20:05:49 +0000

    f latest

--- f/flake.nix
+++ f/flake.nix
@@ -1,14 +1,17 @@
 {
-  inputs.pkg.url = github:defn/pkg/0.0.121;
+  inputs.pkg.url = github:defn/pkg/0.0.153;
   outputs = inputs: inputs.pkg.main rec {
     src = ./.;
 
+    extend = pkg: { };
+
     defaultPackage = ctx: ctx.wrap.bashBuilder {
       inherit src;
+
       installPhase = ''
         mkdir -p $out/bin
         cp -a $src/bin/* $out/bin/
-        chmod 755 $out/bin/tf $out/bin/f-*
+        chmod 755 $out/bin/f $out/bin/f-*
       '';
     };
   };
