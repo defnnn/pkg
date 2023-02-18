60a4f886601f31c582366d220c80d2dcc404c720
amanibhavam <iam@defn.sh>
2023-02-18 20:08:16 +0000

    latest

--- az/flake.nix
+++ az/flake.nix
@@ -1,5 +1,6 @@
 {
   inputs = {
+    pkg.url = github:defn/pkg/0.0.153;
     dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
     c.url = github:defn/pkg/c-0.0.4?dir=c;
     n.url = github:defn/pkg/n-0.0.9?dir=n;
@@ -8,26 +9,19 @@
     gum.url = github:defn/pkg/gum-0.8.0-3?dir=gum;
   };
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
+  outputs = inputs: inputs.pkg.main rec {
+    src = ./.;
 
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
+    extend = pkg: { };
 
-    config = rec {
-      slug = builtins.readFile ./SLUG;
-      version = builtins.readFile ./VERSION;
-    };
-
-    handler = { pkgs, wrap, system, builders }: {
-      defaultPackage = wrap.nullBuilder {
-        propagatedBuildInputs = [
-          inputs.c.defaultPackage.${system}
-          inputs.n.defaultPackage.${system}
-          inputs.f.defaultPackage.${system}
-          inputs.tf.defaultPackage.${system}
-          inputs.gum.defaultPackage.${system}
-        ];
-      };
+    defaultPackage = ctx: ctx.wrap.nullBuilder {
+      propagatedBuildInputs = [
+        inputs.c.defaultPackage.${ctx.system}
+        inputs.n.defaultPackage.${ctx.system}
+        inputs.f.defaultPackage.${ctx.system}
+        inputs.tf.defaultPackage.${ctx.system}
+        inputs.gum.defaultPackage.${ctx.system}
+      ];
     };
   };
 }
