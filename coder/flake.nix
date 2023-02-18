074fd7604e1e98d3087db9e090074996d5bcc6c0
amanibhavam <iam@defn.sh>
2023-02-18 19:10:10 +0000

    coder latest

--- coder/flake.nix
+++ coder/flake.nix
@@ -1,71 +1,52 @@
 {
-  inputs = {
-    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
-  };
+  inputs.pkg.url = github:defn/pkg/0.0.150;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
-
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
-
-    config = rec {
-      slug = builtins.readFile ./SLUG;
-      vendor = builtins.readFile ./VENDOR;
-      revision = builtins.readFile ./REVISION;
-      version = "${vendor}-${revision}";
-
-      url_template = input: "${input.url}";
-
-      downloads = {
-        "x86_64-linux" = rec {
-          version = vendor;
-          os = "linux";
-          arch = "amd64";
-          sha256 = "sha256-hgJ/E1UVhxkX+8SUh+83A2FivB3r3iqBj0FdO6lR0Jw="; # x86_64-linux
-          url = "https://github.com/coder/coder/releases/download/v${version}/coder_${version}_${os}_${arch}.tar.gz";
-        };
-        "aarch64-linux" = rec {
-          version = vendor;
-          os = "linux";
-          arch = "arm64";
-          sha256 = "sha256-3JwtSZg2jx6fAR7FU3e1GbpD9WnPR0IJdeknU0kxmxE="; # aarch64-linux
-          url = "https://github.com/coder/coder/releases/download/v${version}/coder_${version}_${os}_${arch}.tar.gz";
-        };
-        "x86_64-darwin" = rec {
-          version = vendor;
-          os = "darwin";
-          arch = "amd64";
-          sha256 = "sha256-PeWxJTr9pAIz4Bpw3V8xklop8/+V4V1KMvGXDTx4Cr8="; # x86_64-darwin
-          url = "https://github.com/coder/coder/releases/download/v${version}/coder_${version}_${os}_${arch}.zip";
-        };
-        "aarch64-darwin" = rec {
-          version = vendor;
-          os = "darwin";
-          arch = "arm64";
-          sha256 = "sha256-1ZvyuOqJtQUL/k+uuPEGbICWGZxdrAypjfrjjZnEFHw="; # aarch64-darwin
-          url = "https://github.com/coder/coder/releases/download/v${version}/coder_${version}_${os}_${arch}.zip";
-        };
+    extend = { ctx, this }: {
+      apps.default = {
+        type = "app";
+        program = "${this.defaultPackage}/bin/coder";
       };
-
-      installPhase = { src }: ''
-        install -m 0755 -d $out $out/bin $out/lib
-        cp coder $out/bin/coder
-      '';
     };
 
-    handler = { pkgs, wrap, system, builders }:
-      let
-        this = wrap.genDownloadBuilders
-          {
-            dontFixup = true;
-            buildInputs = with pkgs; [ unzip ];
-          };
-      in
-      this // {
-        apps.default = {
-          type = "app";
-          program = "${this.defaultPackage}/bin/coder";
-        };
+    url_template = input:
+      if input.os == "linux" then
+        "https://github.com/coder/coder/releases/download/v${input.vendor}/coder_${input.vendor}_${input.os}_${input.arch}.tar.gz"
+      else
+        "https://github.com/coder/coder/releases/download/v${input.vendor}/coder_${input.vendor}_${input.os}_${input.arch}.zip";
+
+    installPhase = { src }: ''
+      install -m 0755 -d $out $out/bin $out/lib
+      cp coder $out/bin/coder
+    '';
+
+    downloads = {
+      options = { ctx }: {
+        dontFixup = true;
+        buildInputs = with ctx.pkgs; [ unzip ];
       };
+
+      "x86_64-linux" = rec {
+        os = "linux";
+        arch = "amd64";
+        sha256 = "sha256-hgJ/E1UVhxkX+8SUh+83A2FivB3r3iqBj0FdO6lR0Jw="; # x86_64-linux
+      };
+      "aarch64-linux" = rec {
+        os = "linux";
+        arch = "arm64";
+        sha256 = "sha256-3JwtSZg2jx6fAR7FU3e1GbpD9WnPR0IJdeknU0kxmxE="; # aarch64-linux
+      };
+      "x86_64-darwin" = rec {
+        os = "darwin";
+        arch = "amd64";
+        sha256 = "sha256-PeWxJTr9pAIz4Bpw3V8xklop8/+V4V1KMvGXDTx4Cr8="; # x86_64-darwin
+      };
+      "aarch64-darwin" = rec {
+        os = "darwin";
+        arch = "arm64";
+        sha256 = "sha256-1ZvyuOqJtQUL/k+uuPEGbICWGZxdrAypjfrjjZnEFHw="; # aarch64-darwin
+      };
+    };
   };
 }
