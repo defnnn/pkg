06287b5976a9020dcca946266fbaf729f0d40b1e
amanibhavam <iam@defn.sh>
2023-02-19 02:49:50 +0000

    c latest

--- c/flake.nix
+++ c/flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.pkg.url = github:defn/pkg/0.0.153;
+  inputs.pkg.url = github:defn/pkg/0.0.156;
   inputs.cue.url = "github:defn/pkg/hof-0.6.7-4?dir=hof";
   inputs.hof.url = "github:defn/pkg/cue-0.5.0-beta.5-1?dir=cue";
   outputs = inputs: inputs.pkg.main rec {
