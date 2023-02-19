0b6a2cfb6c25945aab69c9fadd2befd7b76bc975
amanibhavam <iam@defn.sh>
2023-02-19 02:58:33 +0000

    kubernetes latest

--- kubernetes/flake.nix
+++ kubernetes/flake.nix
@@ -1,30 +1,23 @@
 {
   inputs = {
-    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
-    k3d.url = github:defn/pkg/k3d-5.4.7-0?dir=k3d;
-    kubectl.url = github:defn/pkg/kubectl-1.25.6-0?dir=kubectl;
-    k9s.url = github:defn/pkg/k9s-0.27.2-0?dir=k9s;
-    helm.url = github:defn/pkg/helm-3.11.1-0?dir=helm;
-    kustomize.url = github:defn/pkg/kustomize-5.0.0-0?dir=kustomize;
-    argo.url = github:defn/pkg/argo-3.4.5-0?dir=argo;
-    argocd.url = github:defn/pkg/argocd-2.6.1-0?dir=argocd;
-    argorollouts.url = github:defn/pkg/argorollouts-1.4.0-3?dir=argorollouts;
+    pkg.url = github:defn/pkg/0.0.156;
+    k3d.url = github:defn/pkg/k3d-5.4.7-2?dir=k3d;
+    kubectl.url = github:defn/pkg/kubectl-1.25.6-2?dir=kubectl;
+    k9s.url = github:defn/pkg/k9s-0.27.2-3?dir=k9s;
+    helm.url = github:defn/pkg/helm-3.11.1-9?dir=helm;
+    kustomize.url = github:defn/pkg/kustomize-5.0.0-7?dir=kustomize;
+    argo.url = github:defn/pkg/argo-3.4.5-3?dir=argo;
+    argocd.url = github:defn/pkg/argocd-2.6.1-3?dir=argocd;
+    argorollouts.url = github:defn/pkg/argorollouts-1.4.0-5?dir=argorollouts;
   };
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
+  outputs = inputs: inputs.pkg.main rec {
+    src = ./.;
 
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
+    extend = pkg: { };
 
-    config = rec {
-      slug = builtins.readFile ./SLUG;
-      version = builtins.readFile ./VERSION;
-    };
-
-    handler = { pkgs, wrap, system, builders }: rec {
-      defaultPackage = wrap.nullBuilder {
-        propagatedBuildInputs = wrap.flakeInputs;
-      };
+    defaultPackage = ctx: ctx.wrap.nullBuilder {
+      propagatedBuildInputs = ctx.wrap.flakeInputs;
     };
   };
 }
