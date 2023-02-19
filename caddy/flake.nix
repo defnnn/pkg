ea9d26be8d28ba6a45057a310f32254776d8fe39
amanibhavam <iam@defn.sh>
2023-02-19 02:43:57 +0000

    caddy latest

--- caddy/flake.nix
+++ caddy/flake.nix
@@ -1,16 +1,20 @@
 {
-  inputs.pkg.url = github:defn/pkg/0.0.141;
+  inputs.pkg.url = github:defn/pkg/0.0.156;
   outputs = inputs: inputs.pkg.downloadMain rec {
     src = ./.;
 
+    extend = pkg: { };
+
     url_template = input: "https://github.com/caddyserver/caddy/releases/download/v${input.vendor}/caddy_${input.vendor}_${input.os}_${input.arch}.tar.gz";
 
-    installPhase = { src }: ''
+    installPhase = pkg: ''
       install -m 0755 -d $out $out/bin
       install -m 0755 caddy $out/bin/caddy
     '';
 
     downloads = {
+      options = pkg: { };
+
       "x86_64-linux" = {
         os = "linux";
         arch = "amd64";
