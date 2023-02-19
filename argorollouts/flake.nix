e8a4205018062bff5178423c71c6e4bfaa68d7c6
amanibhavam <iam@defn.sh>
2023-02-19 02:43:18 +0000

    argorollouts latest

--- argorollouts/flake.nix
+++ argorollouts/flake.nix
@@ -1,17 +1,19 @@
 {
-  inputs.pkg.url = github:defn/pkg/0.0.141;
+  inputs.pkg.url = github:defn/pkg/0.0.156;
   outputs = inputs: inputs.pkg.downloadMain rec {
     src = ./.;
 
+    extend = pkg: { };
+
     url_template = input: "https://github.com/argoproj/argo-rollouts/releases/download/v${input.vendor}/kubectl-argo-rollouts-${input.os}-${input.arch}";
 
-    installPhase = { src }: ''
+    installPhase = pkg: ''
       install -m 0755 -d $out $out/bin
       install -m 0755 $src $out/bin/argo-rollouts
     '';
 
     downloads = {
-      options = {
+      options = pkg: {
         dontUnpack = true;
       };
 
