ccf7ec474362863ea6e7b80603df14c80b2a6076
amanibhavam <iam@defn.sh>
2023-02-19 07:20:46 +0000

    cloud propagate flakes

--- cloud/flake.nix
+++ cloud/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    pkg.url = github:defn/pkg/0.0.159;
+    pkg.url = github:defn/pkg/0.0.166;
     awscli.url = github:defn/pkg/awscli-2.10.0-3?dir=awscli;
     flyctl.url = github:defn/pkg/flyctl-0.0.456-2?dir=flyctl;
   };
@@ -9,7 +9,14 @@
     src = ./.;
 
     defaultPackage = ctx: ctx.wrap.nullBuilder {
-      propagatedBuildInputs = ctx.wrap.flakeInputs;
+      propagatedBuildInputs =
+        let
+          flakeInputs = [
+            inputs.awscli.defaultPackage.${ctx.system}
+            inputs.flyctl.defaultPackage.${ctx.system}
+          ];
+        in
+        flakeInputs;
     };
   };
 }
