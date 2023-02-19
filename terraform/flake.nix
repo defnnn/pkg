e092973abd1b9d3d95de9350b4960c2ccd8bdd33
amanibhavam <iam@defn.sh>
2023-02-19 02:38:28 +0000

    terraform latest

--- terraform/flake.nix
+++ terraform/flake.nix
@@ -1,60 +1,45 @@
 {
-  inputs = {
-    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
-  };
+  inputs.pkg.url = github:defn/pkg/0.0.156;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
+    extend = pkg: { };
 
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
+    url_template = input: "https://releases.hashicorp.com/terraform/${input.vendor}/terraform_${input.vendor}_${input.os}_${input.arch}.zip";
 
-    config = rec {
-      slug = builtins.readFile ./SLUG;
-      vendor = builtins.readFile ./VENDOR;
-      revision = builtins.readFile ./REVISION;
-      version = "${vendor}-${revision}";
+    installPhase = pkg: ''
+      install -m 0755 -d $out $out/bin
+      unzip $src
+      install -m 0755 terraform $out/bin/terraform
+    '';
 
-      url_template = input: "https://releases.hashicorp.com/terraform/${input.version}/terraform_${input.version}_${input.os}_${input.arch}.zip";
-
-      downloads = {
-        "x86_64-linux" = {
-          version = vendor;
-          os = "linux";
-          arch = "amd64";
-          sha256 = "sha256-vHXivBro9ty2P5sl1dB+P4pAbGK4fDaWceRHYKxzkiA="; # x86_64-linux
-        };
-        "aarch64-linux" = {
-          version = vendor;
-          os = "linux";
-          arch = "arm64";
-          sha256 = "sha256-RBC67ayDONVgyQ2n7ikSjnq8Zrhoo5KntYbzX0H7ViQ="; # aarch64-linux
-        };
-        "x86_64-darwin" = {
-          version = vendor;
-          os = "darwin";
-          arch = "amd64";
-          sha256 = "sha256-2nDLx0I2sfsCpCymtFnhKlTtKKk78AbaG/kNakV7iXc="; # x86_64-darwin
-        };
-        "aarch64-darwin" = {
-          version = vendor;
-          os = "darwin";
-          arch = "arm64";
-          sha256 = "sha256-W85iuoqXI8dRceAQ8TNkOKFmVqsG2Pbw0bo7PUC3C5g="; # aarch64-darwin
-        };
-      };
-
-      installPhase = { src }: ''
-        install -m 0755 -d $out $out/bin
-        unzip $src
-        install -m 0755 terraform $out/bin/terraform
-      '';
-    };
-
-    handler = { pkgs, wrap, system, builders }:
-      wrap.genDownloadBuilders {
+    downloads = {
+      options = pkg: {
         dontUnpack = true;
         dontFixup = true;
-        buildInputs = [ pkgs.unzip ];
+        buildInputs = with pkg.ctx.pkgs; [ unzip ];
       };
+
+      "x86_64-linux" = {
+        os = "linux";
+        arch = "amd64";
+        sha256 = "sha256-vHXivBro9ty2P5sl1dB+P4pAbGK4fDaWceRHYKxzkiA="; # x86_64-linux
+      };
+      "aarch64-linux" = {
+        os = "linux";
+        arch = "arm64";
+        sha256 = "sha256-RBC67ayDONVgyQ2n7ikSjnq8Zrhoo5KntYbzX0H7ViQ="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "darwin";
+        arch = "amd64";
+        sha256 = "sha256-2nDLx0I2sfsCpCymtFnhKlTtKKk78AbaG/kNakV7iXc="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "darwin";
+        arch = "arm64";
+        sha256 = "sha256-W85iuoqXI8dRceAQ8TNkOKFmVqsG2Pbw0bo7PUC3C5g="; # aarch64-darwin
+      };
+    };
   };
 }
