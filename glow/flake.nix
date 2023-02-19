8599210f4d63d4c9b38cb21aa9018af3f84219af
amanibhavam <iam@defn.sh>
2023-02-19 02:37:15 +0000

    glow latest

--- glow/flake.nix
+++ glow/flake.nix
@@ -1,17 +1,19 @@
 {
-  inputs.pkg.url = github:defn/pkg/0.0.142;
+  inputs.pkg.url = github:defn/pkg/0.0.156;
   outputs = inputs: inputs.pkg.downloadMain rec {
     src = ./.;
 
+    extend = pkg: { };
+
     url_template = input: "https://github.com/charmbracelet/glow/releases/download/v${input.vendor}/glow_${input.vendor}_${input.os}_${input.arch}.tar.gz";
 
-    installPhase = { src }: ''
+    installPhase = pkg: ''
       install -m 0755 -d $out $out/bin
       install -m 0755 glow $out/bin/glow
     '';
 
     downloads = {
-      options = { };
+      options = pkg: { };
 
       "x86_64-linux" = {
         os = "Linux";
