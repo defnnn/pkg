a5c1c3194ba006acec9a88e6d99ff412165ce8f7
amanibhavam <iam@defn.sh>
2023-02-19 02:39:36 +0000

    vhs latest

--- vhs/flake.nix
+++ vhs/flake.nix
@@ -1,17 +1,19 @@
 {
-  inputs.pkg.url = github:defn/pkg/0.0.141;
+  inputs.pkg.url = github:defn/pkg/0.0.156;
   outputs = inputs: inputs.pkg.downloadMain rec {
     src = ./.;
 
+    extend = pkg: { };
+
     url_template = input: "https://github.com/charmbracelet/vhs/releases/download/v${input.vendor}/vhs_${input.vendor}_${input.os}_${input.arch}.tar.gz";
 
-    installPhase = { src }: ''
+    installPhase = pkg: ''
       install -m 0755 -d $out $out/bin
       install -m 0755 vhs $out/bin/vhs
     '';
 
     downloads = {
-      options = { };
+      options = pkg: { };
 
       "x86_64-linux" = {
         os = "Linux";
