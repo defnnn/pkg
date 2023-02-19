098c5f1e10ec6615e2261637560d466c03207c76
amanibhavam <iam@defn.sh>
2023-02-19 02:43:37 +0000

    buf latest

--- buf/flake.nix
+++ buf/flake.nix
@@ -1,16 +1,20 @@
 {
-  inputs.pkg.url = github:defn/pkg/0.0.141;
+  inputs.pkg.url = github:defn/pkg/0.0.156;
   outputs = inputs: inputs.pkg.downloadMain rec {
     src = ./.;
 
+    extend = pkg: { };
+
     url_template = input: "https://github.com/bufbuild/buf/releases/download/v${input.vendor}/buf-${input.os}-${input.arch}.tar.gz";
 
-    installPhase = { src }: ''
+    installPhase = pkg: ''
       install -m 0755 -d $out $out/bin
       install -m 0755 buf/bin/buf $out/bin/buf
     '';
 
     downloads = {
+      options = pkg: { };
+
       "x86_64-linux" = {
         os = "Linux";
         arch = "x86_64";
