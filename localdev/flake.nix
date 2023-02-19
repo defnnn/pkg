26ca948fe909107f9c80d373a13f7e47b41de7c3
amanibhavam <iam@defn.sh>
2023-02-19 03:00:39 +0000

    localdev latest

--- localdev/flake.nix
+++ localdev/flake.nix
@@ -2,13 +2,13 @@
   inputs = {
     pkg.url = github:defn/pkg/0.0.156;
     latest.url = github:NixOS/nixpkgs?rev=4938e72add339f76d795284cb5a3aae85d02ee53;
-    caddy.url = github:defn/pkg/caddy-2.6.3-0?dir=caddy;
-    coder.url = github:defn/pkg/coder-0.17.3-0?dir=coder;
-    codeserver.url = github:defn/pkg/codeserver-4.10.0-rc.2-0?dir=codeserver;
-    terraform.url = github:defn/pkg/terraform-1.3.4?dir=terraform;
-    earthly.url = github:defn/pkg/earthly-0.7.0-rc2-0?dir=earthly;
-    tilt.url = github:defn/pkg/tilt-0.31.2-0?dir=tilt;
-    gh.url = github:defn/pkg/gh-2.23.0-0?dir=gh;
+    caddy.url = github:defn/pkg/caddy-2.6.3-2?dir=caddy;
+    coder.url = github:defn/pkg/coder-0.17.3-2?dir=coder;
+    codeserver.url = github:defn/pkg/codeserver-4.10.0-2?dir=codeserver;
+    terraform.url = github:defn/pkg/terraform-1.4.0-beta2-1?dir=terraform;
+    earthly.url = github:defn/pkg/earthly-0.7.0-rc3-1?dir=earthly;
+    tilt.url = github:defn/pkg/tilt-0.31.2-2?dir=tilt;
+    gh.url = github:defn/pkg/gh-2.23.0-2?dir=gh;
   };
 
   outputs = inputs: inputs.pkg.main rec {
