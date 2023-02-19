c8fc2a473e65a4336121cbd9f7e233db0f0d04a7
amanibhavam <iam@defn.sh>
2023-02-19 02:48:29 +0000

    coder latest

--- coder/flake.nix
+++ coder/flake.nix
@@ -1,12 +1,12 @@
 {
-  inputs.pkg.url = github:defn/pkg/0.0.150;
+  inputs.pkg.url = github:defn/pkg/0.0.156;
   outputs = inputs: inputs.pkg.downloadMain rec {
     src = ./.;
 
-    extend = { ctx, this }: {
+    extend = pkg: {
       apps.default = {
         type = "app";
-        program = "${this.defaultPackage}/bin/coder";
+        program = "${pkg.this.defaultPackage}/bin/coder";
       };
     };
 
@@ -16,33 +16,33 @@
       else
         "https://github.com/coder/coder/releases/download/v${input.vendor}/coder_${input.vendor}_${input.os}_${input.arch}.zip";
 
-    installPhase = { src }: ''
+    installPhase = pkg: ''
       install -m 0755 -d $out $out/bin $out/lib
       cp coder $out/bin/coder
     '';
 
     downloads = {
-      options = { ctx }: {
+      options = pkg: {
         dontFixup = true;
-        buildInputs = with ctx.pkgs; [ unzip ];
+        buildInputs = with pkg.ctx.pkgs; [ unzip ];
       };
 
-      "x86_64-linux" = rec {
+      "x86_64-linux" = {
         os = "linux";
         arch = "amd64";
         sha256 = "sha256-hgJ/E1UVhxkX+8SUh+83A2FivB3r3iqBj0FdO6lR0Jw="; # x86_64-linux
       };
-      "aarch64-linux" = rec {
+      "aarch64-linux" = {
         os = "linux";
         arch = "arm64";
         sha256 = "sha256-3JwtSZg2jx6fAR7FU3e1GbpD9WnPR0IJdeknU0kxmxE="; # aarch64-linux
       };
-      "x86_64-darwin" = rec {
+      "x86_64-darwin" = {
         os = "darwin";
         arch = "amd64";
         sha256 = "sha256-PeWxJTr9pAIz4Bpw3V8xklop8/+V4V1KMvGXDTx4Cr8="; # x86_64-darwin
       };
-      "aarch64-darwin" = rec {
+      "aarch64-darwin" = {
         os = "darwin";
         arch = "arm64";
         sha256 = "sha256-1ZvyuOqJtQUL/k+uuPEGbICWGZxdrAypjfrjjZnEFHw="; # aarch64-darwin
