781bd687e5e4f945cedfd0ac11c25f00597c1d00
amanibhavam <iam@defn.sh>
2023-02-19 02:35:52 +0000

    acme latest

--- acme/flake.nix
+++ acme/flake.nix
@@ -1,60 +1,40 @@
 {
-  inputs = {
-    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
-  };
+  inputs.pkg.url = github:defn/pkg/0.0.156;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
+    extend = pkg: {
+      sourceRoot = ".";
 
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
-
-    config = rec {
-      slug = builtins.readFile ./SLUG;
-      vendor = builtins.readFile ./VENDOR;
-      revision = builtins.readFile ./REVISION;
-      version ="${vendor}-${revision}";
-
-      url_template = input: "https://github.com/acmesh-official/acme.sh/archive/refs/tags/v${input.version}.tar.gz";
-
-      downloads = {
-        "x86_64-linux" = {
-          version = vendor;
-          sha256 = "sha256-oZEy7WVJFAmo2Kk8Jk3LzyZakdRaRt5nSBG5P1mVOY8"; # x86_64-linux
-        };
-        "aarch64-linux" = {
-          version = vendor;
-          sha256 = "sha256-oZEy7WVJFAmo2Kk8Jk3LzyZakdRaRt5nSBG5P1mVOY8"; # aarch64-linux
-        };
-        "x86_64-darwin" = {
-          version = vendor;
-          sha256 = "sha256-oZEy7WVJFAmo2Kk8Jk3LzyZakdRaRt5nSBG5P1mVOY8"; # x86_64-darwin
-        };
-        "aarch64-darwin" = {
-          version = vendor;
-          sha256 = "sha256-oZEy7WVJFAmo2Kk8Jk3LzyZakdRaRt5nSBG5P1mVOY8"; # aarch64-darwin
-        };
+      apps.default = {
+        type = "app";
+        program = "${pkg.this.defaultPackage}/bin/acme.sh";
       };
-
-      installPhase = { src }: ''
-        install -m 0755 -d $out $out/bin $out/bin/dnsapi
-        install -m 0755 */acme.sh $out/bin/acme.sh
-        cp */dnsapi/* $out/bin/dnsapi/
-      '';
     };
 
-    handler = { pkgs, wrap, system, builders }:
-      let
-        builds = wrap.genDownloadBuilders { };
-      in
-      builds // {
-        apps.default = {
-          type = "app";
-          program = "${builds.defaultPackage}/bin/acme.sh";
-        };
-      } // {
-        defaultPackage = builds.defaultPackage // {
-          sourceRoot = ".";
-        };
+    url_template = input: "https://github.com/acmesh-official/acme.sh/archive/refs/tags/v${input.vendor}.tar.gz";
+
+    installPhase = pkg: ''
+      install -m 0755 -d $out $out/bin $out/bin/dnsapi
+      install -m 0755 */acme.sh $out/bin/acme.sh
+      cp */dnsapi/* $out/bin/dnsapi/
+    '';
+
+    downloads = {
+      options = pkg: { };
+
+      "x86_64-linux" = {
+        sha256 = "sha256-oZEy7WVJFAmo2Kk8Jk3LzyZakdRaRt5nSBG5P1mVOY8"; # x86_64-linux
       };
+      "aarch64-linux" = {
+        sha256 = "sha256-oZEy7WVJFAmo2Kk8Jk3LzyZakdRaRt5nSBG5P1mVOY8"; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        sha256 = "sha256-oZEy7WVJFAmo2Kk8Jk3LzyZakdRaRt5nSBG5P1mVOY8"; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        sha256 = "sha256-oZEy7WVJFAmo2Kk8Jk3LzyZakdRaRt5nSBG5P1mVOY8"; # aarch64-darwin
+      };
+    };
   };
 }
