5d954e5aeba234dfb9c2c04046c0d16e0b06d8e1
amanibhavam <iam@defn.sh>
2023-02-19 02:41:03 +0000

    gh latest

--- gh/flake.nix
+++ gh/flake.nix
@@ -1,16 +1,20 @@
 {
-  inputs.pkg.url = github:defn/pkg/0.0.141;
+  inputs.pkg.url = github:defn/pkg/0.0.156;
   outputs = inputs: inputs.pkg.downloadMain rec {
     src = ./.;
 
+    extend = pkg: { };
+
     url_template = input: "https://github.com/cli/cli/releases/download/v${input.vendor}/gh_${input.vendor}_${input.os}_${input.arch}.tar.gz";
 
-    installPhase = { src }: ''
+    installPhase = pkg: ''
       install -m 0755 -d $out $out/bin
       install -m 0755 */bin/gh $out/bin/gh
     '';
 
     downloads = {
+      options = pkg: { };
+
       "x86_64-linux" = {
         os = "linux";
         arch = "amd64";
