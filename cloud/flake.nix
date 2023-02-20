997da3029bd5c05900f7e9a227f359cee8e9753b
amanibhavam <iam@defn.sh>
2023-02-20 03:47:51 +0000

    cloud: flyctl 0.0.462

--- cloud/flake.nix
+++ cloud/flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     pkg.url = github:defn/pkg/0.0.166;
     awscli.url = github:defn/pkg/awscli-2.10.0-3?dir=awscli;
-    flyctl.url = github:defn/pkg/flyctl-0.0.456-2?dir=flyctl;
+    flyctl.url = github:defn/pkg/flyctl-0.0.462-0?dir=flyctl;
   };
 
   outputs = inputs: inputs.pkg.main rec {
