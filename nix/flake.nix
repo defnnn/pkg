fa1cd6ac20cfa433ddd4c001268623059e801733
amanibhavam <iam@defn.sh>
2023-02-25 10:57:23 -0800

    nix

---
+++ nix/flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    pkg.url = github:defn/pkg/0.0.166;
+  };
+
+  outputs = inputs: inputs.pkg.main rec {
+    src = ./.;
+
+    defaultPackage = ctx: ctx.wrap.nullBuilder {
+      propagatedBuildInputs = with ctx.pkgs; [
+        direnv
+        nix-direnv
+        nixpkgs-fmt
+      ];
+    };
+  };
+}
