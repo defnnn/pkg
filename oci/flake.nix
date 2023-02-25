0d3ca99e2236ec818c99e27565170df656084286
amanibhavam <iam@defn.sh>
2023-02-25 10:53:17 -0800

    oci tools

---
+++ oci/flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    pkg.url = github:defn/pkg/0.0.166;
+  };
+
+  outputs = inputs: inputs.pkg.main rec {
+    src = ./.;
+
+    defaultPackage = ctx: ctx.wrap.nullBuilder {
+      propagatedBuildInputs = with ctx.pkgs; [
+        docker
+        docker-credential-helpers
+        skopeo
+        dive
+      ];
+    };
+  };
+}
