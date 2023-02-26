71b9b7d5f85ad836da75efa30580666793759d52
amanibhavam <iam@defn.sh>
2023-02-25 21:06:12 -0800

    utils

---
+++ utils/flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    pkg.url = github:defn/pkg/0.0.166;
+  };
+
+  outputs = inputs: inputs.pkg.main rec {
+    src = ./.;
+
+    defaultPackage = ctx: ctx.wrap.nullBuilder {
+      propagatedBuildInputs = with ctx.pkgs; [
+        xz
+        unzip
+        rsync
+        dnsutils
+        nettools
+        htop
+        wget
+        curl
+        procps
+      ];
+    };
+  };
+}
