3b734e8f3c2d581bcd70b71ef2adec98e8bdf936
amanibhavam <iam@defn.sh>
2023-02-25 21:17:28 -0800

    secrets fix

--- secrets/flake.nix
+++ secrets/flake.nix
@@ -10,15 +10,15 @@
       pass = ctx.pkgs.writeShellScriptBin "pass" ''
         { ${ctx.pkgs.pass}/bin/pass "$@" 2>&1 1>&3 3>&- | grep -v 'problem with fast path key listing'; } 3>&1 1>&2 | cat
       '';
+    };
 
-      defaultPackage = ctx: ctx.wrap.nullBuilder {
-        propagatedBuildInputs = with ctx.pkgs; [
-          p.pass
-          gnupg
-          pinentry
-          aws-vault
-        ];
-      };
+    defaultPackage = ctx: ctx.wrap.nullBuilder {
+      propagatedBuildInputs = with ctx.pkgs; [
+        (packages ctx).pass
+        gnupg
+        pinentry
+        aws-vault
+      ];
     };
   };
 }
