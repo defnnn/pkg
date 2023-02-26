f0c987d83555847eae77a8eaeab52965f02446dd
amanibhavam <iam@defn.sh>
2023-02-25 21:08:12 -0800

    shel

---
+++ shell/flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    pkg.url = github:defn/pkg/0.0.166;
+  };
+
+  outputs = inputs: inputs.pkg.main rec {
+    src = ./.;
+
+    defaultPackage = ctx: ctx.wrap.nullBuilder {
+      propagatedBuildInputs = with ctx.pkgs; [
+        vim
+        openssh
+        screen
+        powerline-go
+        less
+        groff
+        bashInteractive
+      ];
+    };
+  };
+}
