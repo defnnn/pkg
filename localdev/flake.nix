c7d9a4dc663fde31cf95376b29dc0e075aea8f5e
amanibhavam <iam@defn.sh>
2023-03-01 20:30:31 -0800

    localdev 0.0.28

--- localdev/flake.nix
+++ localdev/flake.nix
@@ -2,14 +2,14 @@
   inputs = {
     pkg.url = github:defn/pkg/0.0.166;
     latest.url = github:NixOS/nixpkgs?rev=4938e72add339f76d795284cb5a3aae85d02ee53;
-    caddy.url = github:defn/pkg/caddy-2.6.3-2?dir=caddy;
+    caddy.url = github:defn/pkg/caddy-2.6.4-1?dir=caddy;
     coder.url = github:defn/pkg/coder-0.17.4-0?dir=coder;
     codeserver.url = github:defn/pkg/codeserver-4.10.0-2?dir=codeserver;
-    terraform.url = github:defn/pkg/terraform-1.4.0-beta2-1?dir=terraform;
-    earthly.url = github:defn/pkg/earthly-0.7.0-rc3-2?dir=earthly;
+    terraform.url = github:defn/pkg/terraform-1.4.0-rc1-2?dir=terraform;
+    earthly.url = github:defn/pkg/earthly-0.7.1-0?dir=earthly;
     tilt.url = github:defn/pkg/tilt-0.31.2-2?dir=tilt;
     gh.url = github:defn/pkg/gh-2.23.0-2?dir=gh;
-    godev.url = github:defn/pkg/godev-0.0.1?dir=godev;
+    godev.url = github:defn/pkg/godev-0.0.3?dir=godev;
     nodedev.url = github:defn/pkg/nodedev-0.0.1?dir=nodedev;
   };
 
