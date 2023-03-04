5ad64a06e599b77d87bb5a01736af0f99e510561
amanibhavam <iam@defn.sh>
2023-03-04 15:47:30 -0800

    localdev: include buildkite

--- localdev/flake.nix
+++ localdev/flake.nix
@@ -6,9 +6,11 @@
     coder.url = github:defn/pkg/coder-0.17.4-0?dir=coder;
     codeserver.url = github:defn/pkg/codeserver-4.10.0-2?dir=codeserver;
     terraform.url = github:defn/pkg/terraform-1.4.0-rc1-2?dir=terraform;
-    earthly.url = github:defn/pkg/earthly-0.7.1-0?dir=earthly;
     tilt.url = github:defn/pkg/tilt-0.31.2-2?dir=tilt;
     gh.url = github:defn/pkg/gh-2.23.0-2?dir=gh;
+    earthly.url = github:defn/pkg/earthly-0.7.1-0?dir=earthly;
+    buildkite.url = github:defn/pkg/buildkite-3.44.0-1?dir=buildkite;
+    bk.url = github:defn/pkg/bk-2.0.0-0?dir=bk;
   };
 
   outputs = inputs: inputs.pkg.main rec {
@@ -33,9 +35,11 @@
             inputs.coder.defaultPackage.${ctx.system}
             inputs.codeserver.defaultPackage.${ctx.system}
             inputs.terraform.defaultPackage.${ctx.system}
-            inputs.earthly.defaultPackage.${ctx.system}
             inputs.tilt.defaultPackage.${ctx.system}
             inputs.gh.defaultPackage.${ctx.system}
+            inputs.earthly.defaultPackage.${ctx.system}
+            inputs.buildkite.defaultPackage.${ctx.system}
+            inputs.b0k.defaultPackage.${ctx.system}
           ];
         in
         flakeInputs;
