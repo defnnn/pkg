1460749aec2121fca795b7c8e9f50b10fd48c746
amanibhavam <iam@defn.sh>
2023-03-05 20:35:07 -0800

    localdev: dont include coder, code-server

--- localdev/flake.nix
+++ localdev/flake.nix
@@ -1,11 +1,6 @@
 {
   inputs = {
     pkg.url = github:defn/pkg/0.0.166;
-    latest.url = github:NixOS/nixpkgs?rev=4938e72add339f76d795284cb5a3aae85d02ee53;
-    caddy.url = github:defn/pkg/caddy-2.6.4-1?dir=caddy;
-    coder.url = github:defn/pkg/coder-0.17.4-0?dir=coder;
-    codeserver.url = github:defn/pkg/codeserver-4.10.0-2?dir=codeserver;
-    terraform.url = github:defn/pkg/terraform-1.4.0-rc1-2?dir=terraform;
     tilt.url = github:defn/pkg/tilt-0.31.2-2?dir=tilt;
     gh.url = github:defn/pkg/gh-2.23.0-2?dir=gh;
     earthly.url = github:defn/pkg/earthly-0.7.1-0?dir=earthly;
