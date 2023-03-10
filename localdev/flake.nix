275ad85d743b422b7cba15fbff606e81a2380dd6
amanibhavam <iam@defn.sh>
2023-03-09 18:50:54 -0800

    update localdev

--- localdev/flake.nix
+++ localdev/flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     pkg.url = github:defn/pkg/0.0.166;
     tilt.url = github:defn/pkg/tilt-0.31.2-2?dir=tilt;
-    gh.url = github:defn/pkg/gh-2.23.0-2?dir=gh;
+    gh.url = github:defn/pkg/gh-2.24.3-0?dir=gh;
     earthly.url = github:defn/pkg/earthly-0.7.1-0?dir=earthly;
     buildkite.url = github:defn/pkg/buildkite-3.44.0-1?dir=buildkite;
     bk.url = github:defn/pkg/bk-2.0.0-0?dir=bk;
@@ -10,17 +10,6 @@
 
   outputs = inputs: inputs.pkg.main rec {
     src = ./.;
-
-    apps = ctx: {
-      coder = {
-        type = "app";
-        program = "${inputs.coder.defaultPackage.${ctx.system}}/bin/coder";
-      };
-      codeserver = {
-        type = "app";
-        program = "${inputs.codeserver.defaultPackage.${ctx.system}}/bin/code-server";
-      };
-    };
 
     defaultPackage = ctx: ctx.wrap.nullBuilder {
       propagatedBuildInputs =
