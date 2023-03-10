0ec8b471c026ee5ce5d6dd2d7a6149cdd034635b
amanibhavam <iam@defn.sh>
2023-03-09 19:02:43 -0800

    update kubernetes

--- kubernetes/flake.nix
+++ kubernetes/flake.nix
@@ -1,10 +1,10 @@
 {
   inputs = {
     pkg.url = github:defn/pkg/0.0.166;
-    k3d.url = github:defn/pkg/k3d-5.4.7-2?dir=k3d;
+    k3d.url = github:defn/pkg/k3d-5.4.8-0?dir=k3d;
     kubectl.url = github:defn/pkg/kubectl-1.25.6-2?dir=kubectl;
-    k9s.url = github:defn/pkg/k9s-0.27.2-3?dir=k9s;
-    helm.url = github:defn/pkg/helm-3.11.1-9?dir=helm;
+    k9s.url = github:defn/pkg/k9s-0.27.3-0?dir=k9s;
+    helm.url = github:defn/pkg/helm-3.11.2-0?dir=helm;
     kustomize.url = github:defn/pkg/kustomize-5.0.0-7?dir=kustomize;
     argo.url = github:defn/pkg/argo-3.4.5-3?dir=argo;
     argocd.url = github:defn/pkg/argocd-2.6.1-3?dir=argocd;
