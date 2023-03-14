d9ae8c7f0458df60b1e71b556d594309f942f094
amanibhavam <iam@defn.sh>
2023-03-14 05:19:42 +0000

    cloud: include step

--- cloud/flake.nix
+++ cloud/flake.nix
@@ -4,6 +4,7 @@
     awscli.url = github:defn/pkg/awscli-2.10.0-3?dir=awscli;
     terraform.url = github:defn/pkg/terraform-1.4.0-0?dir=terraform;
     packer.url = github:defn/pkg/packer-1.8.6-0?dir=packer;
+    step.url = github:defn/pkg/step-0.23.4-1?dir=step;
   };
 
   outputs = inputs: inputs.pkg.main rec {
@@ -16,6 +17,7 @@
             inputs.awscli.defaultPackage.${ctx.system}
             inputs.terraform.defaultPackage.${ctx.system}
             inputs.packer.defaultPackage.${ctx.system}
+            inputs.step.defaultPackage.${ctx.system}
           ];
         in
         flakeInputs;
