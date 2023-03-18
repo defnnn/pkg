40a4bf276836d11b488de73253dc9298942c4e2a
amanibhavam <iam@defn.sh>
2023-03-18 21:20:44 +0000

    stuff

--- localdev/flake.nix
+++ localdev/flake.nix
@@ -1,14 +1,14 @@
 {
   inputs = {
-    pkg.url = github:defn/pkg/0.0.168;
-    tilt.url = github:defn/pkg/tilt-0.31.2-3?dir=tilt;
-    nomad.url = github:defn/pkg/nomad-1.5.0-1?dir=nomad;
-    boundary.url = github:defn/pkg/boundary-0.12.0-1?dir=boundary;
-    vault.url = github:defn/pkg/vault-1.13.0-1?dir=vault;
+    pkg.url = github:defn/pkg/0.0.169;
+    tilt.url = github:defn/pkg/tilt-0.31.2-4?dir=tilt;
+    nomad.url = github:defn/pkg/nomad-1.5.0-2?dir=nomad;
+    boundary.url = github:defn/pkg/boundary-0.12.0-2?dir=boundary;
+    vault.url = github:defn/pkg/vault-1.13.0-2?dir=vault;
     gh.url = github:defn/pkg/gh-gh-2.20.2-3?dir=gh;
-    earthly.url = github:defn/pkg/earthly-0.7.1-1?dir=earthly;
-    buildkite.url = github:defn/pkg/buildkite-3.44.0-2?dir=buildkite;
-    bk.url = github:defn/pkg/bk-2.0.0-1?dir=bk;
+    earthly.url = github:defn/pkg/earthly-0.7.1-2?dir=earthly;
+    buildkite.url = github:defn/pkg/buildkite-3.44.0-3?dir=buildkite;
+    bk.url = github:defn/pkg/bk-2.0.0-2?dir=bk;
   };
 
   outputs = inputs: inputs.pkg.main rec {
