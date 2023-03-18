3a894daa067e2e27e5bb1ae708de44ddd182826e
amanibhavam <iam@defn.sh>
2023-03-18 20:40:30 +0000

    auto upgrade, fix slug cloud/

--- cloud/flake.nix
+++ cloud/flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
-    pkg.url = github:defn/pkg/0.0.166;
+    pkg.url = github:defn/pkg/0.0.167;
     awscli.url = github:defn/pkg/awscli-2.10.0-3?dir=awscli;
-    terraform.url = github:defn/pkg/terraform-1.4.0-0?dir=terraform;
+    terraform.url = github:defn/pkg/terraform-1.4.0-rc1-2?dir=terraform;
     packer.url = github:defn/pkg/packer-1.8.6-0?dir=packer;
     step.url = github:defn/pkg/step-0.23.4-1?dir=step;
   };
