25fd3cb1ff50ec6c905425a43d213ede9471e9ff
amanibhavam <iam@defn.sh>
2023-03-18 21:47:24 +0000

    stuff

--- c/flake.nix
+++ c/flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.pkg.url = github:defn/pkg/0.0.169;
+  inputs.pkg.url = github:defn/pkg/0.0.170;
   inputs.cue.url = "github:defn/pkg/hof-0.6.8-beta.12-0?dir=hof";
   inputs.hof.url = "github:defn/pkg/cue-0.5.0-beta.5-3?dir=cue";
   outputs = inputs: inputs.pkg.main rec {
