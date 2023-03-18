c69d15b80abf3ad446262bd7e02d0850e4cb83ee
amanibhavam <iam@defn.sh>
2023-03-18 18:42:47 +0000

    hof v0.6.8-beta.12

--- hof/flake.nix
+++ hof/flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.pkg.url = github:defn/pkg/0.0.166;
+  inputs.pkg.url = github:defn/pkg/0.0.167;
   outputs = inputs: inputs.pkg.downloadMain rec {
     src = ./.;
 
@@ -16,22 +16,22 @@
       "x86_64-linux" = {
         os = "Linux";
         arch = "x86_64";
-        sha256 = "sha256-9ETOkA8WYQNEIzDzGnuRoNtS57HFwgyyssKIwjRHuEU="; # x86_64-linux
+        sha256 = "sha256-RDi87muYLkxIPH5mHg8jzFkTZLJo0DFytXscIGmlekQ="; # x86_64-linux
       };
       "aarch64-linux" = {
         os = "Linux";
         arch = "arm64";
-        sha256 = "sha256-c6opqw2Gjl5icjjTorblJKQRI8qAJAS+hQ3H9vUKlWE="; # aarch64-linux
+        sha256 = "sha256-4GdksRjuZiMRNkTUKKLz/70hTlDl1HcIGQVe8E50q+Y="; # aarch64-linux
       };
       "x86_64-darwin" = {
         os = "Darwin";
         arch = "x86_64";
-        sha256 = "sha256-S0C0spCHPTzSjK8EUQIo0FmjS66lTTyXiSgrjTFOwEo="; # x86_64-darwin
+        sha256 = "sha256-IbDHK9uMsT4oReRY75XwxJj7uFFQTDiVB4Ao8gDJIGE="; # x86_64-darwin
       };
       "aarch64-darwin" = {
         os = "darwin";
         arch = "arm64";
-        sha256 = "sha256-JtXaTqFNpv5F7hUykVlNjRZef6k8JppWfFLy+QPNpbE="; # x86_64-darwin
+        sha256 = "sha256-IbDHK9uMsT4oReRY75XwxJj7uFFQTDiVB4Ao8gDJIGE="; # x86_64-darwin
       };
     };
   };
