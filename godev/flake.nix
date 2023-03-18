cd1b4cf33aa498e17ec3475dcffa52340cc916f7
amanibhavam <iam@defn.sh>
2023-03-18 18:07:14 +0000

    fix cosign

--- godev/flake.nix
+++ godev/flake.nix
@@ -5,7 +5,7 @@
     buf.url = github:defn/pkg/buf-1.15.1-0?dir=buf;
     operatorsdk.url = github:defn/pkg/operatorsdk-1.27.0-0?dir=operatorsdk;
     goreleaser.url = github:defn/pkg/goreleaser-1.16.1-0?dir=goreleaser;
-    cosign.url = github:defn/pkg/cosign-2.0.0-0?dir=cosign;
+    cosign.url = github:defn/pkg/cosign-2.0.0-1?dir=cosign;
     latest.url = github:NixOS/nixpkgs?rev=64c27498901f104a11df646278c4e5c9f4d642db; # nixos-unstable https://lazamar.co.uk/nix-versions/
   };
 
