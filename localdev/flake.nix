8a1d467e468c078e20db721d661f03f1116095a9
amanibhavam <iam@defn.sh>
2023-03-19 00:39:55 +0000

    localdev: bump gh

--- localdev/flake.nix
+++ localdev/flake.nix
@@ -5,7 +5,7 @@
     nomad.url = github:defn/pkg/nomad-1.5.0-2?dir=nomad;
     boundary.url = github:defn/pkg/boundary-0.12.0-2?dir=boundary;
     vault.url = github:defn/pkg/vault-1.13.0-2?dir=vault;
-    gh.url = github:defn/pkg/gh-gh-2.20.2-3?dir=gh;
+    gh.url = github:defn/pkg/gh-2.24.3-4?dir=gh;
     earthly.url = github:defn/pkg/earthly-0.7.1-2?dir=earthly;
     buildkite.url = github:defn/pkg/buildkite-3.44.0-3?dir=buildkite;
     bk.url = github:defn/pkg/bk-2.0.0-3?dir=bk;
