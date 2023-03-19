f1d89e9a7ec45d3bd78a547890e2a8eee4f01fc2
amanibhavam <iam@defn.sh>
2023-03-19 17:44:53 +0000

    initial honeyvent

---
+++ honeyvent/flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs.pkg.url = github:defn/pkg/0.0.170;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
+
+    url_template = input: "https://github.com/honeycombio/honeyvent/releases/download/v${input.vendor}/honeyvent-${input.os}-${input.arch}";
+
+    installPhase = pkg: ''
+      install -m 0755 -d $out $out/bin
+      install -m 0755 $src $out/bin/honeyvent
+    '';
+
+    downloads = {
+      options = pkg: {
+        dontUnpack = true;
+        buildInputs = with pkg.ctx.pkgs; [ unzip ];
+      };
+
+      "x86_64-linux" = {
+        os = "linux";
+        arch = "amd64";
+        sha256 = "sha256-OBCtbXCDbVtPLvXeJ8PIo+1PNbszFjUTfUQiPihdb8U="; # x86_64-linux
+      };
+      "aarch64-linux" = {
+        os = "linux";
+        arch = "arm64";
+        sha256 = "sha256-pIC+KlaiDkhZjW359oJnMRXOfuT1hkaw4H5Batfaw8c="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "darwin";
+        arch = "amd64";
+        sha256 = "sha256-yayquKSKozRf0yPEMVyKrKUrL2zkxvg7b6FizUxRZyU="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "darwin";
+        arch = "amd64";
+        sha256 = "sha256-yayquKSKozRf0yPEMVyKrKUrL2zkxvg7b6FizUxRZyU="; # aarch64-darwin
+      };
+    };
+  };
+}
