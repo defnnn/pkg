349eed39095e7f973a2dac0e8a01990fc14a0ab8
amanibhavam <iam@defn.sh>
2023-03-19 06:55:39 +0000

    all in terms of all.sh

--- .buildkite/all.sh
+++ .buildkite/all.sh
@@ -3,9 +3,13 @@
 function main {
     set -eu
 
+    if [[ "$#" == 0 ]]; then
+        set -- . */
+    fi
+
     echo "steps:"
 
-    for D in .. ../*/; do
+    for D in "$@"; do
         case "$D" in
             */all/)
                 true
