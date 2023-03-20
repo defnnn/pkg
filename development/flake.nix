9453fe0125913806f83905ebbf8658b0ffd2d0eb
amanibhavam <iam@defn.sh>
2023-03-20 01:19:56 +0000

    switch to stable bazel 6.0.0

--- development/flake.nix
+++ development/flake.nix
@@ -1,13 +1,14 @@
 {
   inputs = {
     pkg.url = github:defn/pkg/0.0.170;
+    latest.url = github:NixOS/nixpkgs?rev=8ad5e8132c5dcf977e308e7bf5517cc6cc0bf7d8; # nixos-unstable https://lazamar.co.uk/nix-versions/
   };
 
   outputs = inputs: inputs.pkg.main rec {
     src = ./.;
 
     defaultPackage = ctx: ctx.wrap.nullBuilder {
-      propagatedBuildInputs = with ctx.pkgs; [
+      propagatedBuildInputs = with (import inputs.latest { system = ctx.system; }); [
         gnumake
         git
         git-lfs
