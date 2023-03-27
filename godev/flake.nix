28830d04a56e4117202f1d60f836f876592fc74d
amanibhavam <iam@defn.sh>
2023-03-27 10:48:41 -0700

    upgrade deps

--- godev/flake.nix
+++ godev/flake.nix
@@ -2,8 +2,8 @@
   inputs = {
     pkg.url = github:defn/pkg/0.0.170;
     buf.url = github:defn/pkg/buf-1.15.1-2?dir=buf;
-    operatorsdk.url = github:defn/pkg/operatorsdk-1.27.0-2?dir=operatorsdk;
-    goreleaser.url = github:defn/pkg/goreleaser-1.16.1-2?dir=goreleaser;
+    operatorsdk.url = github:defn/pkg/operatorsdk-1.28.0-1?dir=operatorsdk;
+    goreleaser.url = github:defn/pkg/goreleaser-1.16.2-1?dir=goreleaser;
     cosign.url = github:defn/pkg/cosign-2.0.0-4?dir=cosign;
     latest.url = github:NixOS/nixpkgs?rev=64c27498901f104a11df646278c4e5c9f4d642db; # nixos-unstable https://lazamar.co.uk/nix-versions/
   };
