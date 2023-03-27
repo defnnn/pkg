f548d6cc939105efda2a6b26b5c07f11f97ae396
amanibhavam <iam@defn.sh>
2023-03-27 10:46:13 -0700

    operatorsdk: upgrade 1.28.0

--- operatorsdk/flake.nix
+++ operatorsdk/flake.nix
@@ -19,22 +19,22 @@
       "x86_64-linux" = {
         os = "linux";
         arch = "amd64";
-        sha256 = "sha256-2VM3LUFC+FkE8WE4gfPXjB+1OQREWJ9UAp4zzH447hM="; # x86_64-linux
+        sha256 = "sha256-2ltXxZ9cl1HARmVplo014a0Rgwj08Kn9OPgAEwz4Xos="; # x86_64-linux
       };
       "aarch64-linux" = {
         os = "linux";
         arch = "arm64";
-        sha256 = "sha256-FZzKcWxk5h5e0gHP+U/u6gMGGu6RekVrC51/QdYYRcQ="; # aarch64-linux
+        sha256 = "sha256-qZ7vth9NZiZs4zf4CWH0vbS2yL1c8MLQH0QIHxNZhG4="; # aarch64-linux
       };
       "x86_64-darwin" = {
         os = "darwin";
         arch = "amd64";
-        sha256 = "sha256-TrctLApmoODFxbSbadfVEnkQKfD2FU35uJEZ2Q/8Nk8="; # x86_64-darwin
+        sha256 = "sha256-lvWFvd7boiNNtFvn8aue48xrxnaOeNBBmQm6d52B2es="; # x86_64-darwin
       };
       "aarch64-darwin" = {
         os = "darwin";
         arch = "arm64";
-        sha256 = "sha256-TDTBSXbmkFOhVuahzzzOzlbw0SlZPmw0u7n3iz/gBjA="; # aarch64-darwin
+        sha256 = "sha256-dJcLK8Pl2wjX2zGIvZFaz9eBm381ssf6L0qjJqjkHdM="; # aarch64-darwin
       };
     };
   };
