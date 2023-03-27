7c9cc4a64daa733a0e376252b70e89ed59386baa
amanibhavam <iam@defn.sh>
2023-03-27 09:48:05 -0700

    az: n: add n-latest

--- az/flake.nix
+++ az/flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     pkg.url = github:defn/pkg/0.0.170;
     c.url = github:defn/pkg/c-0.0.11?dir=c;
-    n.url = github:defn/pkg/n-0.0.32?dir=n;
+    n.url = github:defn/pkg/n-0.0.33?dir=n;
     tf.url = github:defn/pkg/tf-0.0.8?dir=tf;
     gum.url = github:defn/pkg/gum-0.9.0-4?dir=gum;
     glow.url = github:defn/pkg/glow-1.5.0-4?dir=glow;
