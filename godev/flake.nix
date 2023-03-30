d004d368f4688791f8be609645ffe522799843e9
amanibhavam <iam@defn.sh>
2023-03-30 06:29:57 +0000

    upgrade buf

--- godev/flake.nix
+++ godev/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     pkg.url = github:defn/pkg/0.0.172;
-    buf.url = github:defn/pkg/buf-1.15.1-2?dir=buf;
+    buf.url = github:defn/pkg/buf-1.16.0-0?dir=buf;
     operatorsdk.url = github:defn/pkg/operatorsdk-1.28.0-1?dir=operatorsdk;
     goreleaser.url = github:defn/pkg/goreleaser-1.16.2-1?dir=goreleaser;
     cosign.url = github:defn/pkg/cosign-2.0.0-4?dir=cosign;
