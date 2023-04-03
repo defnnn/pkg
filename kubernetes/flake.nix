9642b3e05e97682e8ed495998db011d723e11188
amanibhavam <iam@defn.sh>
2023-04-03 13:03:11 +0000

    upgrade pkg, dev

--- kubernetes/flake.nix
+++ kubernetes/flake.nix
@@ -1,17 +1,17 @@
 {
   inputs = {
-    pkg.url = github:defn/pkg/0.0.194;
+    pkg.url = github:defn/pkg/0.0.195;
     k3d.url = github:defn/pkg/k3d-5.4.9-22?dir=k3d;
     kubectl.url = github:defn/pkg/kubectl-1.25.8-22?dir=kubectl;
     k9s.url = github:defn/pkg/k9s-0.27.3-24?dir=k9s;
     helm.url = github:defn/pkg/helm-3.11.2-25?dir=helm;
-    kustomize.url = github:defn/pkg/kustomize-5.0.1-22?dir=kustomize;
-    stern.url = github:defn/pkg/stern-1.24.0-23?dir=stern;
+    kustomize.url = github:defn/pkg/kustomize-5.0.1-23?dir=kustomize;
+    stern.url = github:defn/pkg/stern-1.24.0-24?dir=stern;
     argo.url = github:defn/pkg/argo-3.4.6-22?dir=argo;
     argocd.url = github:defn/pkg/argocd-2.6.7-23?dir=argocd;
-    tkn.url = github:defn/pkg/tkn-0.30.0-24?dir=tkn;
+    tkn.url = github:defn/pkg/tkn-0.30.0-25?dir=tkn;
     kn.url = github:defn/pkg/kn-1.9.2-23?dir=kn;
-    vcluster.url = github:defn/pkg/vcluster-0.14.2-22?dir=vcluster;
+    vcluster.url = github:defn/pkg/vcluster-0.14.2-23?dir=vcluster;
   };
 
   outputs = inputs: inputs.pkg.main rec {
