73ce0965a3b76a08f7e8ec65ea80064cd2bc639b
amanibhavam <iam@defn.sh>
2023-04-03 12:25:58 +0000

    upgrade pkg, dev

--- kubernetes/flake.nix
+++ kubernetes/flake.nix
@@ -1,16 +1,16 @@
 {
   inputs = {
     pkg.url = github:defn/pkg/0.0.192;
-    k3d.url = github:defn/pkg/k3d-5.4.9-19?dir=k3d;
-    kubectl.url = github:defn/pkg/kubectl-1.25.8-19?dir=kubectl;
-    k9s.url = github:defn/pkg/k9s-0.27.3-21?dir=k9s;
-    helm.url = github:defn/pkg/helm-3.11.2-22?dir=helm;
+    k3d.url = github:defn/pkg/k3d-5.4.9-20?dir=k3d;
+    kubectl.url = github:defn/pkg/kubectl-1.25.8-20?dir=kubectl;
+    k9s.url = github:defn/pkg/k9s-0.27.3-22?dir=k9s;
+    helm.url = github:defn/pkg/helm-3.11.2-23?dir=helm;
     kustomize.url = github:defn/pkg/kustomize-5.0.1-20?dir=kustomize;
     stern.url = github:defn/pkg/stern-1.24.0-21?dir=stern;
-    argo.url = github:defn/pkg/argo-3.4.6-19?dir=argo;
-    argocd.url = github:defn/pkg/argocd-2.6.7-20?dir=argocd;
+    argo.url = github:defn/pkg/argo-3.4.6-20?dir=argo;
+    argocd.url = github:defn/pkg/argocd-2.6.7-21?dir=argocd;
     tkn.url = github:defn/pkg/tkn-0.30.0-22?dir=tkn;
-    kn.url = github:defn/pkg/kn-1.9.2-20?dir=kn;
+    kn.url = github:defn/pkg/kn-1.9.2-21?dir=kn;
     vcluster.url = github:defn/pkg/vcluster-0.14.2-20?dir=vcluster;
   };
 
--- localdev/flake.nix
+++ localdev/flake.nix
@@ -3,16 +3,16 @@
     pkg.url = github:defn/pkg/0.0.192;
     tilt.url = github:defn/pkg/tilt-0.32.0-20?dir=tilt;
     nomad.url = github:defn/pkg/nomad-1.5.2-20?dir=nomad;
-    boundary.url = github:defn/pkg/boundary-0.12.1-19?dir=boundary;
+    boundary.url = github:defn/pkg/boundary-0.12.1-20?dir=boundary;
     vault.url = github:defn/pkg/vault-1.13.1-20?dir=vault;
-    gh.url = github:defn/pkg/gh-2.25.1-20?dir=gh;
-    earthly.url = github:defn/pkg/earthly-0.7.2-19?dir=earthly;
-    buildkite.url = github:defn/pkg/buildkite-3.45.0-19?dir=buildkite;
-    bk.url = github:defn/pkg/bk-2.0.0-22?dir=bk;
-    buildevents.url = github:defn/pkg/buildevents-0.13.0-19?dir=buildevents;
-    honeyvent.url = github:defn/pkg/honeyvent-1.1.3-19?dir=honeyvent;
-    honeymarker.url = github:defn/pkg/honeymarker-0.2.10-21?dir=honeymarker;
-    honeytail.url = github:defn/pkg/honeytail-1.8.2-20?dir=honeytail;
+    gh.url = github:defn/pkg/gh-2.25.1-21?dir=gh;
+    earthly.url = github:defn/pkg/earthly-0.7.2-20?dir=earthly;
+    buildkite.url = github:defn/pkg/buildkite-3.45.0-20?dir=buildkite;
+    bk.url = github:defn/pkg/bk-2.0.0-23?dir=bk;
+    buildevents.url = github:defn/pkg/buildevents-0.13.0-20?dir=buildevents;
+    honeyvent.url = github:defn/pkg/honeyvent-1.1.3-20?dir=honeyvent;
+    honeymarker.url = github:defn/pkg/honeymarker-0.2.10-22?dir=honeymarker;
+    honeytail.url = github:defn/pkg/honeytail-1.8.2-21?dir=honeytail;
   };
 
   outputs = inputs: inputs.pkg.main rec {
