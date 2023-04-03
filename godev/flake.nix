2965676fc57d1a4ede552430b88a93c7a171c391
amanibhavam <iam@defn.sh>
2023-04-03 08:53:08 +0000

    upgrade pkg, dev

--- godev/flake.nix
+++ godev/flake.nix
@@ -1,10 +1,10 @@
 {
   inputs = {
     pkg.url = github:defn/pkg/0.0.176;
-    buf.url = github:defn/pkg/buf-1.16.0-3?dir=buf;
+    buf.url = github:defn/pkg/buf-1.16.0-4?dir=buf;
     operatorsdk.url = github:defn/pkg/operatorsdk-1.28.0-5?dir=operatorsdk;
     goreleaser.url = github:defn/pkg/goreleaser-1.16.2-5?dir=goreleaser;
-    cosign.url = github:defn/pkg/cosign-2.0.0-7?dir=cosign;
+    cosign.url = github:defn/pkg/cosign-2.0.0-8?dir=cosign;
     latest.url = github:NixOS/nixpkgs?rev=64c27498901f104a11df646278c4e5c9f4d642db; # nixos-unstable https://lazamar.co.uk/nix-versions/
   };
 
--- localdev/flake.nix
+++ localdev/flake.nix
@@ -3,16 +3,16 @@
     pkg.url = github:defn/pkg/0.0.176;
     tilt.url = github:defn/pkg/tilt-0.32.0-4?dir=tilt;
     nomad.url = github:defn/pkg/nomad-1.5.2-4?dir=nomad;
-    boundary.url = github:defn/pkg/boundary-0.12.1-3?dir=boundary;
+    boundary.url = github:defn/pkg/boundary-0.12.1-4?dir=boundary;
     vault.url = github:defn/pkg/vault-1.13.1-4?dir=vault;
-    gh.url = github:defn/pkg/gh-2.25.1-4?dir=gh;
-    earthly.url = github:defn/pkg/earthly-0.7.2-3?dir=earthly;
-    buildkite.url = github:defn/pkg/buildkite-3.45.0-3?dir=buildkite;
-    bk.url = github:defn/pkg/bk-2.0.0-6?dir=bk;
-    buildevents.url = github:defn/pkg/buildevents-0.13.0-3?dir=buildevents;
-    honeyvent.url = github:defn/pkg/honeyvent-1.1.3-3?dir=honeyvent;
-    honeymarker.url = github:defn/pkg/honeymarker-0.2.10-5?dir=honeymarker;
-    honeytail.url = github:defn/pkg/honeytail-1.8.2-4?dir=honeytail;
+    gh.url = github:defn/pkg/gh-2.25.1-5?dir=gh;
+    earthly.url = github:defn/pkg/earthly-0.7.2-4?dir=earthly;
+    buildkite.url = github:defn/pkg/buildkite-3.45.0-4?dir=buildkite;
+    bk.url = github:defn/pkg/bk-2.0.0-7?dir=bk;
+    buildevents.url = github:defn/pkg/buildevents-0.13.0-4?dir=buildevents;
+    honeyvent.url = github:defn/pkg/honeyvent-1.1.3-4?dir=honeyvent;
+    honeymarker.url = github:defn/pkg/honeymarker-0.2.10-6?dir=honeymarker;
+    honeytail.url = github:defn/pkg/honeytail-1.8.2-5?dir=honeytail;
   };
 
   outputs = inputs: inputs.pkg.main rec {
