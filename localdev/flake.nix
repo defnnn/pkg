5bcd21e28f2c8bbe93ce35fc2fbaefe72f9b0a5a
amanibhavam <iam@defn.sh>
2023-04-03 08:33:01 +0000

    upgrade pkg, dev

--- localdev/flake.nix
+++ localdev/flake.nix
@@ -1,10 +1,10 @@
 {
   inputs = {
-    pkg.url = github:defn/pkg/0.0.174;
-    tilt.url = github:defn/pkg/tilt-0.32.0-2?dir=tilt;
-    nomad.url = github:defn/pkg/nomad-1.5.2-2?dir=nomad;
+    pkg.url = github:defn/pkg/0.0.175;
+    tilt.url = github:defn/pkg/tilt-0.32.0-3?dir=tilt;
+    nomad.url = github:defn/pkg/nomad-1.5.2-3?dir=nomad;
     boundary.url = github:defn/pkg/boundary-0.12.1-2?dir=boundary;
-    vault.url = github:defn/pkg/vault-1.13.1-2?dir=vault;
+    vault.url = github:defn/pkg/vault-1.13.1-3?dir=vault;
     gh.url = github:defn/pkg/gh-2.25.1-3?dir=gh;
     earthly.url = github:defn/pkg/earthly-0.7.2-2?dir=earthly;
     buildkite.url = github:defn/pkg/buildkite-3.45.0-2?dir=buildkite;
