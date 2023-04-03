6cf5617205c9ddc52e14a84e2cbc10edce196a50
amanibhavam <iam@defn.sh>
2023-04-03 09:28:53 +0000

    upgrade pkg, dev

--- cloud/flake.nix
+++ cloud/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     pkg.url = github:defn/pkg/0.0.179;
-    awscli.url = github:defn/pkg/awscli-2.11.8-6?dir=awscli;
+    awscli.url = github:defn/pkg/awscli-2.11.8-7?dir=awscli;
     terraform.url = github:defn/pkg/terraform-1.4.4-7?dir=terraform;
     packer.url = github:defn/pkg/packer-1.8.6-9?dir=packer;
     step.url = github:defn/pkg/step-0.23.4-10?dir=step;
--- kubernetes/flake.nix
+++ kubernetes/flake.nix
@@ -1,16 +1,16 @@
 {
   inputs = {
     pkg.url = github:defn/pkg/0.0.179;
-    k3d.url = github:defn/pkg/k3d-5.4.9-6?dir=k3d;
-    kubectl.url = github:defn/pkg/kubectl-1.25.8-6?dir=kubectl;
-    k9s.url = github:defn/pkg/k9s-0.27.3-8?dir=k9s;
-    helm.url = github:defn/pkg/helm-3.11.2-9?dir=helm;
+    k3d.url = github:defn/pkg/k3d-5.4.9-7?dir=k3d;
+    kubectl.url = github:defn/pkg/kubectl-1.25.8-7?dir=kubectl;
+    k9s.url = github:defn/pkg/k9s-0.27.3-9?dir=k9s;
+    helm.url = github:defn/pkg/helm-3.11.2-10?dir=helm;
     kustomize.url = github:defn/pkg/kustomize-5.0.1-7?dir=kustomize;
     stern.url = github:defn/pkg/stern-1.24.0-8?dir=stern;
-    argo.url = github:defn/pkg/argo-3.4.6-6?dir=argo;
-    argocd.url = github:defn/pkg/argocd-2.6.7-7?dir=argocd;
+    argo.url = github:defn/pkg/argo-3.4.6-7?dir=argo;
+    argocd.url = github:defn/pkg/argocd-2.6.7-8?dir=argocd;
     tkn.url = github:defn/pkg/tkn-0.30.0-9?dir=tkn;
-    kn.url = github:defn/pkg/kn-1.9.2-7?dir=kn;
+    kn.url = github:defn/pkg/kn-1.9.2-8?dir=kn;
     vcluster.url = github:defn/pkg/vcluster-0.14.2-7?dir=vcluster;
   };
 
