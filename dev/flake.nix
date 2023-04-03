3eabf82d97d0247f42611149bfa95a847651b36e
amanibhavam <iam@defn.sh>
2023-04-03 01:39:15 +0000

    merge wrapper into dev

--- dev/flake.nix
+++ dev/flake.nix
@@ -3,11 +3,6 @@
     nixpkgs.url = github:NixOS/nixpkgs?rev=4d2b37a84fad1091b9de401eb450aae66f1a741e;
     flake-utils.url = github:numtide/flake-utils?rev=04c1b180862888302ddfb2e3ad9eaa63afc60cf8;
 
-    wrapper = {
-      url = github:defn/pkg/wrapper-0.0.23?dir=wrapper;
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     gomod2nix = {
       url = github:defn/gomod2nix/;
       inputs.nixpkgs.follows = "nixpkgs";
@@ -18,7 +13,97 @@
   outputs = inputs:
     let
       prelude = rec {
-        wrapper = inputs.wrapper;
+        wrapper = {
+          wrap = { other, system, site }:
+            let
+              pkgs = import inputs.nixpkgs { inherit system; };
+              hasDefaultPackage = (item: acc:
+                acc ++
+                (
+                  if item ? ${"slug"}
+                  then
+                    (
+                      (if item.slug.${system} == site.slug
+                      then [ ]
+                      else
+                        (if item ? ${"defaultPackage"}
+                        then [ item.defaultPackage.${system} ]
+                        else [ ]))
+                    )
+                  else
+                    (
+                      if item ? ${"defaultPackage"}
+                      then [ item.defaultPackage.${system} ]
+                      else [ ]
+                    )
+                ));
+            in
+            rec {
+              devShell = { devInputs ? [ ] }:
+                pkgs.stdenvNoCC.mkDerivation {
+                  name = "${site.slug}-shell-${site.version}";
+                  buildInputs =
+                    [ other.self.defaultPackage.${system} ]
+                    ++ devInputs;
+                };
+
+              genDownloadBuilders = commonBuild: {
+                defaultPackage = downloadBuilder commonBuild;
+
+                packages = pkgs.lib.genAttrs [ "aarch64-linux" "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ]
+                  (name: downloadBuilder (commonBuild // {
+                    overrideSystem = name;
+                  }));
+              };
+
+              downloadBuilder = { propagatedBuildInputs ? [ ], buildInputs ? [ ], dontUnpack ? false, dontFixup ? false, overrideSystem ? system, config }:
+                pkgs.stdenv.mkDerivation rec {
+                  inherit dontUnpack;
+                  inherit dontFixup;
+
+                  inherit propagatedBuildInputs;
+                  inherit buildInputs;
+
+                  name = "${site.slug}-${site.version}";
+
+                  src = with site.downloads.${overrideSystem}; pkgs.fetchurl {
+                    url = site.url_template (config // site.downloads.${overrideSystem});
+                    inherit sha256;
+                  };
+
+                  sourceRoot = ".";
+
+                  installPhase = site.installPhase { inherit src; inherit config; };
+                };
+
+              bashBuilder = input@{ propagatedBuildInputs ? [ ], buildInputs ? [ ], src, installPhase, dontUnpack ? false, dontFixup ? false, slug ? "${site.slug}" }:
+                pkgs.stdenv.mkDerivation rec {
+                  name = "${slug}-${site.version}";
+
+                  inherit dontUnpack;
+                  inherit dontFixup;
+
+                  inherit propagatedBuildInputs;
+                  inherit buildInputs;
+
+                  src = input.src;
+                  installPhase = input.installPhase;
+                };
+
+              nullBuilder = input@{ propagatedBuildInputs ? [ ], buildInputs ? [ ], dontUnpack ? false, dontFixup ? false, slug ? "null-builder" }:
+                bashBuilder rec {
+                  inherit dontUnpack;
+                  inherit dontFixup;
+
+                  inherit propagatedBuildInputs;
+                  inherit buildInputs;
+
+                  src = builtins.path { path = ./.; name = slug; };
+
+                  installPhase = "mkdir -p $out";
+                };
+            };
+        };
 
         eachDefaultSystem = inputs.flake-utils.lib.eachDefaultSystem;
 
@@ -124,3 +209,4 @@
       };
     });
 }
+
