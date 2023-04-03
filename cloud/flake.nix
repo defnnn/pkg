f21521f67628e7dbb63b232c01137f33763a30b4
amanibhavam <iam@defn.sh>
2023-04-03 12:08:30 +0000

    upgrade pkg, dev

--- cloud/flake.nix
+++ cloud/flake.nix
@@ -1,10 +1,10 @@
 {
   inputs = {
-    pkg.url = github:defn/pkg/0.0.190;
+    pkg.url = github:defn/pkg/0.0.191;
     awscli.url = github:defn/pkg/awscli-2.11.8-18?dir=awscli;
-    terraform.url = github:defn/pkg/terraform-1.4.4-18?dir=terraform;
-    packer.url = github:defn/pkg/packer-1.8.6-20?dir=packer;
-    step.url = github:defn/pkg/step-0.23.4-21?dir=step;
+    terraform.url = github:defn/pkg/terraform-1.4.4-19?dir=terraform;
+    packer.url = github:defn/pkg/packer-1.8.6-21?dir=packer;
+    step.url = github:defn/pkg/step-0.23.4-22?dir=step;
   };
 
   outputs = inputs: inputs.pkg.main rec {
