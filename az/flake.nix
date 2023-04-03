b1c11d80695efc1022a7a389cf09e5e4d4fd799e
amanibhavam <iam@defn.sh>
2023-04-03 09:53:15 +0000

    upgrade pkg, dev

--- az/flake.nix
+++ az/flake.nix
@@ -1,11 +1,11 @@
 {
   inputs = {
-    pkg.url = github:defn/pkg/0.0.180;
-    c.url = github:defn/pkg/c-0.4.12?dir=c;
-    n.url = github:defn/pkg/n-0.0.55?dir=n;
-    tf.url = github:defn/pkg/tf-0.0.16?dir=tf;
-    gum.url = github:defn/pkg/gum-0.10.0-9?dir=gum;
-    glow.url = github:defn/pkg/glow-1.5.0-12?dir=glow;
+    pkg.url = github:defn/pkg/0.0.181;
+    c.url = github:defn/pkg/c-0.4.13?dir=c;
+    n.url = github:defn/pkg/n-0.0.56?dir=n;
+    tf.url = github:defn/pkg/tf-0.0.17?dir=tf;
+    gum.url = github:defn/pkg/gum-0.10.0-10?dir=gum;
+    glow.url = github:defn/pkg/glow-1.5.0-13?dir=glow;
   };
 
   outputs = inputs: inputs.pkg.main rec {
