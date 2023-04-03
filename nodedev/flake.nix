1ca6171d491a7b89fdca9bdcbf1586648e6df3b2
amanibhavam <iam@defn.sh>
2023-04-03 11:31:09 +0000

    upgrade pkg, dev

--- nodedev/flake.nix
+++ nodedev/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    pkg.url = github:defn/pkg/0.0.188;
+    pkg.url = github:defn/pkg/0.0.189;
     latest.url = github:NixOS/nixpkgs?rev=8ad5e8132c5dcf977e308e7bf5517cc6cc0bf7d8; # nixos-unstable https://lazamar.co.uk/nix-versions/
   };
 
