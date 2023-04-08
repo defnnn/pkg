ebface5a23961941efa5066cfe66bf4c384c20e7
amanibhavam <iam@defn.sh>
2023-04-08 02:57:28 +0000

    upgrade dev and deps

--- localdev/flake.nix
+++ localdev/flake.nix
@@ -3,16 +3,16 @@
     pkg.url = github:defn/pkg/0.0.206;
     tilt.url = github:defn/pkg/tilt-0.32.0-32?dir=tilt;
     nomad.url = github:defn/pkg/nomad-1.5.3-6?dir=nomad;
-    boundary.url = github:defn/pkg/boundary-0.12.2-5?dir=boundary;
+    boundary.url = github:defn/pkg/boundary-0.12.2-6?dir=boundary;
     vault.url = github:defn/pkg/vault-1.13.1-32?dir=vault;
-    gh.url = github:defn/pkg/gh-2.27.0-5?dir=gh;
-    earthly.url = github:defn/pkg/earthly-0.7.2-31?dir=earthly;
-    buildkite.url = github:defn/pkg/buildkite-3.45.0-31?dir=buildkite;
-    bk.url = github:defn/pkg/bk-2.0.0-34?dir=bk;
-    buildevents.url = github:defn/pkg/buildevents-0.13.0-31?dir=buildevents;
-    honeyvent.url = github:defn/pkg/honeyvent-1.1.3-31?dir=honeyvent;
-    honeymarker.url = github:defn/pkg/honeymarker-0.2.10-33?dir=honeymarker;
-    honeytail.url = github:defn/pkg/honeytail-1.8.2-32?dir=honeytail;
+    gh.url = github:defn/pkg/gh-2.27.0-6?dir=gh;
+    earthly.url = github:defn/pkg/earthly-0.7.2-32?dir=earthly;
+    buildkite.url = github:defn/pkg/buildkite-3.45.0-32?dir=buildkite;
+    bk.url = github:defn/pkg/bk-2.0.0-35?dir=bk;
+    buildevents.url = github:defn/pkg/buildevents-0.13.0-32?dir=buildevents;
+    honeyvent.url = github:defn/pkg/honeyvent-1.1.3-32?dir=honeyvent;
+    honeymarker.url = github:defn/pkg/honeymarker-0.2.10-34?dir=honeymarker;
+    honeytail.url = github:defn/pkg/honeytail-1.8.2-33?dir=honeytail;
   };
 
   outputs = inputs: inputs.pkg.main rec {
