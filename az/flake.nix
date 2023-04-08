967f27ef7236387e4b4d47d4ecc01865a405e3fe
amanibhavam <iam@defn.sh>
2023-04-08 05:49:04 +0000

    add n-init

--- az/flake.nix
+++ az/flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     pkg.url = github:defn/pkg/0.0.208;
     c.url = github:defn/pkg/c-0.4.38?dir=c;
-    n.url = github:defn/pkg/n-0.0.82?dir=n;
+    n.url = github:defn/pkg/n-0.0.83?dir=n;
     tf.url = github:defn/pkg/tf-0.0.42?dir=tf;
     gum.url = github:defn/pkg/gum-0.10.0-35?dir=gum;
     glow.url = github:defn/pkg/glow-1.5.0-38?dir=glow;
