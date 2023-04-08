9c1488fe357a584adfcb9f5a5d6f2e52cb54a01a
amanibhavam <iam@defn.sh>
2023-04-08 03:10:08 +0000

    upgrade dev and deps

--- cloud/flake.nix
+++ cloud/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     pkg.url = github:defn/pkg/0.0.207;
-    awscli.url = github:defn/pkg/awscli-2.11.11-6?dir=awscli;
+    awscli.url = github:defn/pkg/awscli-2.11.11-7?dir=awscli;
     terraform.url = github:defn/pkg/terraform-1.4.4-33?dir=terraform;
     packer.url = github:defn/pkg/packer-1.8.6-36?dir=packer;
     step.url = github:defn/pkg/step-0.23.4-36?dir=step;
--- localdev/flake.nix
+++ localdev/flake.nix
@@ -3,16 +3,16 @@
     pkg.url = github:defn/pkg/0.0.207;
     tilt.url = github:defn/pkg/tilt-0.32.0-33?dir=tilt;
     nomad.url = github:defn/pkg/nomad-1.5.3-7?dir=nomad;
-    boundary.url = github:defn/pkg/boundary-0.12.2-6?dir=boundary;
+    boundary.url = github:defn/pkg/boundary-0.12.2-7?dir=boundary;
     vault.url = github:defn/pkg/vault-1.13.1-33?dir=vault;
-    gh.url = github:defn/pkg/gh-2.27.0-6?dir=gh;
-    earthly.url = github:defn/pkg/earthly-0.7.2-32?dir=earthly;
-    buildkite.url = github:defn/pkg/buildkite-3.45.0-32?dir=buildkite;
-    bk.url = github:defn/pkg/bk-2.0.0-35?dir=bk;
-    buildevents.url = github:defn/pkg/buildevents-0.13.0-32?dir=buildevents;
-    honeyvent.url = github:defn/pkg/honeyvent-1.1.3-32?dir=honeyvent;
-    honeymarker.url = github:defn/pkg/honeymarker-0.2.10-34?dir=honeymarker;
-    honeytail.url = github:defn/pkg/honeytail-1.8.2-33?dir=honeytail;
+    gh.url = github:defn/pkg/gh-2.27.0-7?dir=gh;
+    earthly.url = github:defn/pkg/earthly-0.7.2-33?dir=earthly;
+    buildkite.url = github:defn/pkg/buildkite-3.45.0-33?dir=buildkite;
+    bk.url = github:defn/pkg/bk-2.0.0-36?dir=bk;
+    buildevents.url = github:defn/pkg/buildevents-0.13.0-33?dir=buildevents;
+    honeyvent.url = github:defn/pkg/honeyvent-1.1.3-33?dir=honeyvent;
+    honeymarker.url = github:defn/pkg/honeymarker-0.2.10-35?dir=honeymarker;
+    honeytail.url = github:defn/pkg/honeytail-1.8.2-34?dir=honeytail;
   };
 
   outputs = inputs: inputs.pkg.main rec {
