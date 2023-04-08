eab8bfabada5d4d3723ae2860fccd3819d291f6f
amanibhavam <iam@defn.sh>
2023-04-08 05:47:13 +0000

    add kubevirt

---
+++ kubevirt/flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs.pkg.url = github:defn/pkg/0.0.208;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
+
+    url_template = input: "https://github.com/kubevirt/kubevirt/releases/download/v${input.vendor}/virtctl-v${input.vendor}-${input.os}-${input.arch}";
+
+    installPhase = pkg: ''
+      install -m 0755 -d $out $out/bin
+      install -m 0755 $src $out/bin/virtctl
+    '';
+
+    downloads = {
+      options = pkg: {
+        dontUnpack = true;
+      };
+
+      "x86_64-linux" = {
+        os = "linux";
+        arch = "amd64";
+        sha256 = "sha256-ItK0fG9ypo8h50XGbzYyAQoEmU/rPm3xZPe8T0tFsL0="; # x86_64-linux
+      };
+      "aarch64-linux" = {
+        os = "linux";
+        arch = "amd64";
+        sha256 = "sha256-ItK0fG9ypo8h50XGbzYyAQoEmU/rPm3xZPe8T0tFsL0="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "darwin";
+        arch = "amd64";
+        sha256 = "sha256-h0FDjuZgfCjW11PZexmUZOEy2Hk8bJiaTLSFwCOmflI="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "darwin";
+        arch = "amd64";
+        sha256 = "sha256-h0FDjuZgfCjW11PZexmUZOEy2Hk8bJiaTLSFwCOmflI="; # aarch64-darwin
+      };
+    };
+  };
+}
