33beb79a5b01a765451eda331d33e44eea4d9548
amanibhavam <iam@defn.sh>
2023-04-09 04:27:15 +0000

    reuse inputs pkg

--- c/flake.nix
+++ c/flake.nix
@@ -1,8 +1,7 @@
 {
-  inputs.pkg.url = github:defn/pkg/0.0.208;
-  inputs.hof.url = "github:defn/pkg/hof-0.6.8-beta.12-4?dir=hof";
   inputs.cue.url = "github:defn/pkg/cue-0.5.0-beta.5-4?dir=cue";
-  outputs = inputs: inputs.pkg.main rec {
+  inputs.hof.url = "github:defn/pkg/hof-0.6.8-beta.12-4?dir=hof";
+  outputs = inputs: inputs.cue.inputs.pkg.main rec {
     src = ./.;
 
     defaultPackage = ctx: ctx.wrap.bashBuilder {
