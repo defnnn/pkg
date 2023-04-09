00e1334f1d5df14c2ab6f80a02356f26a9612162
amanibhavam <iam@defn.sh>
2023-04-09 04:30:23 +0000

    reuse inputs pkg

--- godev/flake.nix
+++ godev/flake.nix
@@ -1,14 +1,13 @@
 {
   inputs = {
-    pkg.url = github:defn/pkg/0.0.208;
+    goreleaser.url = github:defn/pkg/goreleaser-1.16.2-35?dir=goreleaser;
     buf.url = github:defn/pkg/buf-1.17.0-8?dir=buf;
     operatorsdk.url = github:defn/pkg/operatorsdk-1.28.0-35?dir=operatorsdk;
-    goreleaser.url = github:defn/pkg/goreleaser-1.16.2-35?dir=goreleaser;
     cosign.url = github:defn/pkg/cosign-2.0.1-8?dir=cosign;
     latest.url = github:NixOS/nixpkgs?rev=64c27498901f104a11df646278c4e5c9f4d642db; # nixos-unstable https://lazamar.co.uk/nix-versions/
   };
 
-  outputs = inputs: inputs.pkg.main rec {
+  outputs = inputs: inputs.goreleaser.inputs.pkg.main rec {
     src = ./.;
 
     defaultPackage = ctx: ctx.wrap.nullBuilder {
