b9f60584294b2d69e1d4a781509087dd5f74a2ca
amanibhavam <iam@defn.sh>
2023-04-09 04:47:44 +0000

    use app pkg

--- kubernetes/flake.nix
+++ kubernetes/flake.nix
@@ -1,17 +1,17 @@
 {
   inputs = {
-    kubectl.url = github:defn/pkg/kubectl-1.25.8-34?dir=kubectl;
-    k3d.url = github:defn/pkg/k3d-5.4.9-34?dir=k3d;
-    k9s.url = github:defn/pkg/k9s-0.27.3-36?dir=k9s;
-    helm.url = github:defn/pkg/helm-3.11.2-37?dir=helm;
-    kustomize.url = github:defn/pkg/kustomize-5.0.1-34?dir=kustomize;
-    stern.url = github:defn/pkg/stern-1.24.0-35?dir=stern;
-    argo.url = github:defn/pkg/argo-3.4.6-34?dir=argo;
-    argocd.url = github:defn/pkg/argocd-2.6.7-35?dir=argocd;
-    tkn.url = github:defn/pkg/tkn-0.30.0-36?dir=tkn;
-    kn.url = github:defn/pkg/kn-1.9.2-35?dir=kn;
-    vcluster.url = github:defn/pkg/vcluster-0.14.2-34?dir=vcluster;
-    kubevirt.url = github:defn/pkg/kubevirt-0.59.0-1?dir=kubevirt;
+    kubectl.url = github:defn/pkg/kubectl-1.25.8-35?dir=kubectl;
+    k3d.url = github:defn/pkg/k3d-5.4.9-35?dir=k3d;
+    k9s.url = github:defn/pkg/k9s-0.27.3-37?dir=k9s;
+    helm.url = github:defn/pkg/helm-3.11.2-38?dir=helm;
+    kustomize.url = github:defn/pkg/kustomize-5.0.1-35?dir=kustomize;
+    stern.url = github:defn/pkg/stern-1.24.0-36?dir=stern;
+    argo.url = github:defn/pkg/argo-3.4.6-35?dir=argo;
+    argocd.url = github:defn/pkg/argocd-2.6.7-36?dir=argocd;
+    tkn.url = github:defn/pkg/tkn-0.30.0-37?dir=tkn;
+    kn.url = github:defn/pkg/kn-1.9.2-36?dir=kn;
+    vcluster.url = github:defn/pkg/vcluster-0.14.2-35?dir=vcluster;
+    kubevirt.url = github:defn/pkg/kubevirt-0.59.0-2?dir=kubevirt;
   };
 
   outputs = inputs: inputs.kubectl.inputs.pkg.main rec {
