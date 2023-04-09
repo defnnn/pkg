648d82a27d33e811aa9bf8188ca89a738def8a8b
amanibhavam <iam@defn.sh>
2023-04-09 04:50:01 +0000

    use app pkg

--- cloud/flake.nix
+++ cloud/flake.nix
@@ -1,9 +1,9 @@
 {
   inputs = {
-    terraform.url = github:defn/pkg/terraform-1.4.4-34?dir=terraform;
-    packer.url = github:defn/pkg/packer-1.8.6-37?dir=packer;
-    step.url = github:defn/pkg/step-0.23.4-37?dir=step;
-    awscli.url = github:defn/pkg/awscli-2.11.11-8?dir=awscli;
+    terraform.url = github:defn/pkg/terraform-1.4.4-35?dir=terraform;
+    packer.url = github:defn/pkg/packer-1.8.6-38?dir=packer;
+    step.url = github:defn/pkg/step-0.23.4-38?dir=step;
+    awscli.url = github:defn/pkg/awscli-2.11.11-9?dir=awscli;
   };
 
   outputs = inputs: inputs.terraform.inputs.pkg.main rec {
