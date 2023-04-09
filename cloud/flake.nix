96ab5e84c339dd6f5a9ef52005d188426a36f401
amanibhavam <iam@defn.sh>
2023-04-09 04:29:01 +0000

    reuse inputs pkg

--- cloud/flake.nix
+++ cloud/flake.nix
@@ -1,13 +1,12 @@
 {
   inputs = {
-    pkg.url = github:defn/pkg/0.0.208;
-    awscli.url = github:defn/pkg/awscli-2.11.11-8?dir=awscli;
     terraform.url = github:defn/pkg/terraform-1.4.4-34?dir=terraform;
     packer.url = github:defn/pkg/packer-1.8.6-37?dir=packer;
     step.url = github:defn/pkg/step-0.23.4-37?dir=step;
+    awscli.url = github:defn/pkg/awscli-2.11.11-8?dir=awscli;
   };
 
-  outputs = inputs: inputs.pkg.main rec {
+  outputs = inputs: inputs.terraform.inputs.pkg.main rec {
     src = ./.;
 
     defaultPackage = ctx: ctx.wrap.nullBuilder {
